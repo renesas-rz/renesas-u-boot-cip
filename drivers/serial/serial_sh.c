5275991c2d56f87ce33326d76e829c997290bae8
Hisashi Nakamura <hisashi.nakamura.ak@renesas.com>
2013-05-16 15:03:26 +0900

    serial: Modify sys and scif clock frequency of lager
    Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>

--- drivers/serial/serial_sh.c
+++ drivers/serial/serial_sh.c
@@ -62,7 +62,7 @@ static void sh_serial_setbrg(void)
 {
 	DECLARE_GLOBAL_DATA_PTR;
 #if defined(CONFIG_R8A7790)
-	sci_out(&sh_sci, DL, DL_VALUE(gd->baudrate, CONFIG_SYS_CLK_FREQ));
+	sci_out(&sh_sci, DL, DL_VALUE(gd->baudrate, CONFIG_SCIF_CLK_FREQ));
 	udelay((1000000 * 2 * 16 / CONFIG_SYS_CLK_FREQ) * 1000 + 1);
 #else
 	sci_out(&sh_sci, SCBRR, SCBRR_VALUE(gd->baudrate, CONFIG_SYS_CLK_FREQ));
