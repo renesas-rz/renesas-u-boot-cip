ee02b519130fd78f3875337d4c80699a0e649cf1
Yoshiyuki Ito <yoshiyuki.ito.ub@renesas.com>
2014-01-31 18:47:31 +0900

    board: koelsch: Add MMC command support
    Adds support for MMC commands and EXT4 file system.
    
    Signed-off-by: Yoshiyuki Ito <yoshiyuki.ito.ub@renesas.com>

--- board/renesas/koelsch/koelsch.c
+++ board/renesas/koelsch/koelsch.c
@@ -201,6 +201,13 @@ void s_init(void)
 #define	SMSTPCR1	0xE6150134
 #define TMU0_MSTP125	(1 << 25)
 
+#define MSTPSR3		0xE6150048
+#define SMSTPCR3	0xE615011C
+#define MMC0_MSTP315	(1 << 15)
+#define SDHI0_MSTP314	(1 << 14)
+#define SDHI1_MSTP312	(1 << 12)
+#define SDHI2_MSTP311	(1 << 11)
+
 #define	MSTPSR7		0xE61501C4
 #define	SMSTPCR7	0xE615014C
 #define SCIF0_MSTP721	(1 << 21)
@@ -246,6 +253,11 @@ int board_early_init_f(void)
 	val &= ~ETHER_MSTP813;
 	writel(val, SMSTPCR8);
 
+	/* SD */
+	val = readl(MSTPSR3);
+	val &= ~(SDHI0_MSTP314 | SDHI1_MSTP312 | SDHI2_MSTP311);
+	writel(val, SMSTPCR3);
+
 	return 0;
 }
 
@@ -289,6 +301,25 @@ int board_init(void)
 	mdelay(20);
 	gpio_set_value(GPIO_GP_5_22, 1);
 	udelay(1);
+
+	/* sdhi0 */
+	gpio_request(GPIO_GP_7_17, NULL);
+	gpio_request(GPIO_GP_2_12, NULL);
+	gpio_set_value(GPIO_GP_7_17, 1);	/* power on */
+	gpio_set_value(GPIO_GP_2_12, 1);	/* 1: 3.3V, 0: 1.8V */
+	/* sdhi1 */
+	gpio_request(GPIO_GP_7_18, NULL);
+	gpio_request(GPIO_GP_2_13, NULL);
+	gpio_set_value(GPIO_GP_7_18, 1);	/* power on */
+	gpio_set_value(GPIO_GP_2_13, 1);	/* 1: 3.3V, 0: 1.8V */
+	/* sdhi2 */
+	gpio_request(GPIO_GP_7_19, NULL);
+	gpio_request(GPIO_GP_2_26, NULL);
+	gpio_set_value(GPIO_GP_7_19, 1);	/* power on */
+	gpio_set_value(GPIO_GP_2_26, 1);	/* 1: 3.3V, 0: 1.8V */
+
+	/* wait 5ms */
+	udelay(5000);
 
 	return 0;
 }
@@ -338,6 +369,24 @@ int board_late_init(void)
 {
 	return 0;
 }
+
+#ifdef CONFIG_SH_SDHI
+int board_mmc_init(bd_t *bis)
+{
+	int ret;
+
+	/* use SDHI0-2 */
+	ret = sdhi_mmc_init(SDHI0_BASE, 0);
+	if (ret)
+		return ret;
+
+	ret = sdhi_mmc_init(SDHI1_BASE, 1);
+	if (ret)
+		return ret;
+
+	return sdhi_mmc_init(SDHI2_BASE, 2);
+}
+#endif
 
 void reset_cpu(ulong addr)
 {
