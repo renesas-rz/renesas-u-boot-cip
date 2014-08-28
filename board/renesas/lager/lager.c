3763aa535697a611bf5d4b06a64d11bccc6d53bb
Valentine Barshak <valentine.barshak@cogentembedded.com>
2014-08-28 15:47:03 +0400

    board: renesas: lager: Fix build error when SH_MMCIF is disabled
    This reorganizes board_mmc_init() callback to fix build error
    when SH_MMCIF is disabled. We also do not fail immediately
    in case MMCIF initialization fails, but try to initialize
    SDHI controllers if they are enabled.
    
    Signed-off-by: Valentine Barshak <valentine.barshak@cogentembedded.com>

--- board/renesas/lager/lager.c
+++ board/renesas/lager/lager.c
@@ -279,22 +279,22 @@ int board_late_init(void)
 
 int board_mmc_init(bd_t *bis)
 {
-#ifdef CONFIG_SH_SDHI
-	int ret;
+	int ret = 0;
 
+#ifdef CONFIG_SH_MMCIF
 	ret = mmcif_mmc_init();
-	if (ret)
-		return ret;
+#endif
 
+#ifdef CONFIG_SH_SDHI
 	/* use SDHI0/SDHI2 */
 	ret = sdhi_mmc_init(SDHI0_BASE, 0);
 	if (ret)
 		return ret;
 
-	return sdhi_mmc_init(SDHI2_BASE, 2);
-#else
-	return mmcif_mmc_init();
+	ret = sdhi_mmc_init(SDHI2_BASE, 2);
 #endif
+
+	return ret;
 }
 
 void reset_cpu(ulong addr)
