63a51deb81938f34cce369e3e2afd8d9c1ed9def
Kouei Abe <kouei.abe.cp@renesas.com>
2015-01-10 10:31:39 +0900

    board: koelsch: Update to QoS revision 0.411
    This patch updates r8a7791 QoS to revision 0.411.
    
    Signed-off-by: Kouei Abe <kouei.abe.cp@renesas.com>

--- board/renesas/koelsch/qos.c
+++ board/renesas/koelsch/qos.c
@@ -24,7 +24,7 @@
 #include <asm/io.h>
 #include <asm/arch/rmobile.h>
 
-/* QoS version 0.240 for ES1 and version 0.410 for ES2 */
+/* QoS version 0.240 for ES1 and version 0.411 for ES2 later */
 
 #define QOS_PRI_MEDIA	0
 #define QOS_PRI_NORMAL	1
@@ -1407,7 +1407,7 @@ void qos_init_es1(void)
 #endif
 }
 
-/* QoS version 0.410 for ES2 */
+/* QoS version 0.411 for ES2 later */
 
 #define S3CADSPLCR_LINEAR_ALL				0
 #define S3CADSPLCR_4K_SPLIT_LINEAR_0x7000_0x7800	1
@@ -1597,7 +1597,7 @@ void qos_init_es2(void)
 	/* Transaction Control (MXI) */
 	mxi = (struct r8a7791_mxi *)MXI_BASE;
 	writel(0x00000013, &mxi->mxrtcr);
-	writel(0x00000015, &mxi->mxwtcr);
+	writel(0x00000016, &mxi->mxwtcr);
 	writel(0x00780080, &mxi->mxsaar0);
 	writel(0x02000800, &mxi->mxsaar1);
 
