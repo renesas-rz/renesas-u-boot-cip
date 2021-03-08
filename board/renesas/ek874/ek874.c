f09b634264b4bc50823e7cf2e40ed27e7bd008cb
Hien Huynh <hien.huynh.px@renesas.com>
2021-03-08 16:12:57 +0700

    Add support for RZG2E-EK874 board
    Add new support for board EK874 : R8A774C0 RZ/G2E SoC
    
    Refered-work-from : Shinji Hirai <shinji.hirai.zn@renesas.com>
    Signed-off-by: Binh Nguyen <binh.nguyen.uw@renesas.com>
    Signed-off-by: Hien Huynh <hien.huynh.px@renesas.com>

---
+++ board/renesas/ek874/ek874.c
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * board/renesas/ebisu/ebisu.c
+ *     This file is EK874 board support.
+ *
+ * Copyright (C) 2018 Marek Vasut <marek.vasut+renesas@gmail.com>
+ */
+
+#include <common.h>
+#include <cpu_func.h>
+#include <hang.h>
+#include <init.h>
+#include <malloc.h>
+#include <netdev.h>
+#include <dm.h>
+#include <dm/platform_data/serial_sh.h>
+#include <asm/processor.h>
+#include <asm/mach-types.h>
+#include <asm/io.h>
+#include <linux/errno.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/gpio.h>
+#include <asm/arch/gpio.h>
+#include <asm/arch/rmobile.h>
+#include <asm/arch/rcar-mstp.h>
+#include <asm/arch/sh_sdhi.h>
+#include <i2c.h>
+#include <mmc.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+void s_init(void)
+{
+}
+
+int board_early_init_f(void)
+{
+	return 0;
+}
+
+int board_init(void)
+{
+	/* adress of boot parameters */
+	gd->bd->bi_boot_params = CONFIG_SYS_TEXT_BASE + 0x50000;
+
+	return 0;
+}
+
+#define RST_BASE	0xE6160000
+#define RST_CA53RESCNT	(RST_BASE + 0x44)
+#define RST_CA53_CODE	0x5A5A000F
+
+void reset_cpu(ulong addr)
+{
+	writel(RST_CA53_CODE, RST_CA53RESCNT);
+}
+
+void board_add_ram_info(int use_default)
+{
+	int i;
+
+	printf("\n");
+	for (i = 0; i < CONFIG_NR_DRAM_BANKS; i++) {
+		if (!gd->bd->bi_dram[i].size)
+			break;
+		printf("Bank #%d: 0x%09llx - 0x%09llx, ", i,
+		      (unsigned long long)(gd->bd->bi_dram[i].start),
+		      (unsigned long long)(gd->bd->bi_dram[i].start
+		      + gd->bd->bi_dram[i].size - 1));
+		print_size(gd->bd->bi_dram[i].size, "\n");
+	};
+}
---
+++ include/configs/ek874.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * include/configs/ek874.h
+ *     This file is ek874 board configuration.
+ *
+ * Copyright (C) 2019 Renesas Electronics Corporation
+ */
+
+#ifndef __EK874_H
+#define __EK874_H
+
+#undef DEBUG
+
+#include "rcar-gen3-common.h"
+
+/* Ethernet RAVB */
+#define CONFIG_NET_MULTI
+#define CONFIG_BITBANGMII
+#define CONFIG_BITBANGMII_MULTI
+
+/* Generic Timer Definitions (use in assembler source) */
+#define COUNTER_FREQUENCY	0xFE502A	/* 16.66MHz from CPclk */
+
+#endif /* __EK874_H */
