7c30765ded9e5baf9b4f73d29d22ba743933d844
Hien Huynh <hien.huynh.px@renesas.com>
2021-03-09 13:10:20 +0700

    board: renesas: add new support for hihope-rzg2h board
    add new support for Hihope-RZG2H Board : SoC RZG2H
    
    Signed-off-by: Shinji Hirai <shinji.hirai.zn@renesas.com>
    Signed-off-by: Binh Nguyen <binh.nguyen.uw@renesas.com>
    Signed-off-by: Hien Huynh <hien.huynh.px@renesas.com>

---
+++ include/configs/hihope-rzg2h.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * include/configs/hihope-rzg2h.h
+ *     This file is HOPERUN HiHope-RZG2H board configuration.
+ *
+ * Copyright (C) 2020 Renesas Electronics Corporation
+ */
+
+#ifndef __HIHOPE_RZG2H_H
+#define __HIHOPE_RZG2H_H
+
+#include "rcar-gen3-common.h"
+
+/* Ethernet RAVB */
+#define CONFIG_BITBANGMII
+#define CONFIG_BITBANGMII_MULTI
+
+/* Generic Timer Definitions (use in assembler source) */
+#define COUNTER_FREQUENCY	0xFE502A	/* 16.66MHz from CPclk */
+
+#define CONFIG_BOARD_LATE_INIT
+
+#endif /* __HIHOPE_RZG2H_H */
