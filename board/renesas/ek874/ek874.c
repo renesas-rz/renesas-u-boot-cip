24f5053abdb6963375db642c5874c46a6833b6c5
Hien Huynh <hien.huynh.px@renesas.com>
2021-03-10 08:15:39 +0700

    board: ek874: add function to choose correct revision device file
    This commits adds a function to choose correct revision device file
    by checking board revision character.
    
    Signed-off-by: Hien Huynh <hien.huynh.px@renesas.com>

--- board/renesas/ek874/ek874.c
+++ board/renesas/ek874/ek874.c
@@ -128,3 +128,20 @@ void board_add_ram_info(int use_default)
 		print_size(gd->bd->bi_dram[i].size, "\n");
 	};
 }
+
+#if defined(CONFIG_MULTI_DTB_FIT)
+int board_fit_config_name_match(const char *name)
+{
+	char rev;
+
+	rev = check_rev();
+
+	if (!strcmp(name, "r8a774c0-ek874-revc-u-boot") && (rev == 'C'))
+		return 0;
+
+	if (!strcmp(name, "r8a774c0-ek874-u-boot") && (rev == 'E'))
+		return 0;
+
+	return -1;
+}
+#endif
