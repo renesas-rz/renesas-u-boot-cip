abecc18fc67a0311e0ac8a207b50546535446f05
Khoi Nguyen <khoi.nguyen.aj@renesas.com>
2021-05-25 11:13:21 +0700

    net: ravb: Add support Ethernet for G2L
    Adding compatible for Ethernet on RZG2L SMARC
     and Dev board.
    Changing the written values in Ethernet registers
     based on the HW Manual for Ethernet G2L.
    
    Signed-off-by: Khoi Nguyen <khoi.nguyen.aj@renesas.com>
    Signed-off-by: Linh Hua <linh.hua.jc@renesas.com>

--- drivers/net/ravb.c
+++ drivers/net/ravb.c
@@ -36,6 +36,8 @@
 #define RAVB_REG_RIC0		0x360
 #define RAVB_REG_RIC1		0x368
 #define RAVB_REG_RIC2		0x370
+#define RAVB_REG_RIC3		0x388
+#define RAVB_REG_RTC		0x0B4
 #define RAVB_REG_TIC		0x378
 #define RAVB_REG_ECMR		0x500
 #define RAVB_REG_RFLR		0x508
@@ -64,13 +66,21 @@
 #define PIR_MMD			BIT(1)
 #define PIR_MDC			BIT(0)
 
+/* TOE Registers */
+#define CSR0			0x800
+#define CSR0_RPE		0x00000020
+#define CSR0_TPE		0x00000010
+
 #define ECMR_TRCCM		BIT(26)
+#define ECMR_RCPT		BIT(25)
 #define ECMR_RZPF		BIT(20)
 #define ECMR_PFR		BIT(18)
 #define ECMR_RXF		BIT(17)
+#define ECMR_TXF		BIT(16)
 #define ECMR_RE			BIT(6)
 #define ECMR_TE			BIT(5)
 #define ECMR_DM			BIT(1)
+#define ECMR_PRM		BIT(0)
 #define ECMR_CHG_DM		(ECMR_TRCCM | ECMR_RZPF | ECMR_PFR | ECMR_RXF)
 
 /* DMA Descriptors */
@@ -107,6 +117,7 @@
 	 RAVB_RX_DESC_MSC_RTSF | RAVB_RX_DESC_MSC_CEEF)
 
 #define RAVB_TX_TIMEOUT_MS		1000
+#define RAVB_RCV_BUFF_MAX		8192
 
 struct ravb_desc {
 	u32	ctrl;
@@ -360,11 +371,27 @@ static int ravb_write_hwaddr(struct udevice *dev)
 /* E-MAC init function */
 static int ravb_mac_init(struct ravb_priv *eth)
 {
+#if defined(CONFIG_R9A07G044L)
+	struct phy_device *phy = eth->phydev;
+	u32 ecmr;
+
+	ecmr = readl(eth->iobase + RAVB_REG_ECMR);
+
+	ecmr |= ECMR_RZPF | ((phy->duplex) ? ECMR_DM : 0) |
+		ECMR_TE | ECMR_RE | ECMR_RCPT |
+		ECMR_TXF | ECMR_RXF | ECMR_PRM;
+
+	writel(ecmr, eth->iobase + RAVB_REG_ECMR);
+
+	/* Recv frame limit set register */
+	writel(RAVB_RCV_BUFF_MAX + ETH_FCS_LEN, eth->iobase + RAVB_REG_RFLR);
+#else
 	/* Disable MAC Interrupt */
 	writel(0, eth->iobase + RAVB_REG_ECSIPR);
 
 	/* Recv frame limit set register */
 	writel(RFLR_RFL_MIN, eth->iobase + RAVB_REG_RFLR);
+#endif
 
 	return 0;
 }
@@ -388,11 +415,26 @@ static int ravb_dmac_init(struct udevice *dev)
 	writel(0, eth->iobase + RAVB_REG_RIC0);
 	writel(0, eth->iobase + RAVB_REG_RIC1);
 	writel(0, eth->iobase + RAVB_REG_RIC2);
+#if defined(CONFIG_R9A07G044L)
+	writel(0, eth->iobase + RAVB_REG_RIC3);
+#endif
 	writel(0, eth->iobase + RAVB_REG_TIC);
 
 	/* Set little endian */
 	clrbits_le32(eth->iobase + RAVB_REG_CCC, CCC_BOC);
 
+#if defined(CONFIG_R9A07G044L)
+	/* AVB rx set */
+	writel(0x60000000, eth->iobase + RAVB_REG_RCR);
+
+	/* Set Max Frame Length (RTC) */
+	writel(0x7ffc0000 | RAVB_RCV_BUFF_MAX, eth->iobase + RAVB_REG_RTC);
+
+	/* FIFO size set */
+	writel(0x00222200, eth->iobase + RAVB_REG_TGC);
+
+	writel(0, eth->iobase + RAVB_REG_TCCR);
+#else
 	/* AVB rx set */
 	writel(0x18000001, eth->iobase + RAVB_REG_RCR);
 
@@ -439,7 +481,10 @@ static int ravb_config(struct udevice *dev)
 {
 	struct ravb_priv *eth = dev_get_priv(dev);
 	struct phy_device *phy = eth->phydev;
+#if defined(CONFIG_R9A07G044L)
+#else
 	u32 mask = ECMR_CHG_DM | ECMR_RE | ECMR_TE;
+#endif
 	int ret;
 
 	/* Configure AVB-DMAC register */
@@ -449,11 +494,24 @@ static int ravb_config(struct udevice *dev)
 	ravb_mac_init(eth);
 	ravb_write_hwaddr(dev);
 
+#if defined(CONFIG_R9A07G044L)
+	/* Configure TOE registers */
+	writel(CSR0_TPE | CSR0_RPE, eth->iobase + CSR0);
+#endif
+
 	ret = phy_startup(phy);
 	if (ret)
 		return ret;
 
 	/* Set the transfer speed */
+#if defined(CONFIG_R9A07G044L)
+	if (phy->speed == 10)
+		writel(0, eth->iobase + RAVB_REG_GECMR);
+	else if (phy->speed == 100)
+		writel(0x10, eth->iobase + RAVB_REG_GECMR);
+	else if (phy->speed == 1000)
+		writel(0x20, eth->iobase + RAVB_REG_GECMR);
+#else
 	if (phy->speed == 100)
 		writel(0, eth->iobase + RAVB_REG_GECMR);
 	else if (phy->speed == 1000)
@@ -464,6 +522,8 @@ static int ravb_config(struct udevice *dev)
 		mask |= ECMR_DM;
 
 	writel(mask, eth->iobase + RAVB_REG_ECMR);
+
+#endif
 
 	return 0;
 }
@@ -512,8 +572,12 @@ static int ravb_probe(struct udevice *dev)
 	eth->iobase = iobase;
 
 	ret = clk_get_bulk(dev, &eth->clks);
+#if defined(CONFIG_R9A07G044L)
+#else
+	ret = clk_get_by_index(dev, 0, &eth->clk);
 	if (ret < 0)
 		goto err_mdio_alloc;
+#endif
 
 	ret = dev_read_phandle_with_args(dev, "phy-handle", NULL, 0, 0, &phandle_args);
 	if (!ret) {
@@ -543,10 +607,13 @@ static int ravb_probe(struct udevice *dev)
 
 	eth->bus = miiphy_get_dev_by_name(dev->name);
 
+#if defined(CONFIG_R9A07G044L)
+#else
 	/* Bring up PHY */
 	ret = clk_enable_bulk(&eth->clks);
 	if (ret)
 		goto err_mdio_register;
+#endif
 
 	ret = ravb_reset(dev);
 	if (ret)
@@ -693,6 +760,7 @@ int ravb_of_to_plat(struct udevice *dev)
 static const struct udevice_id ravb_ids[] = {
 	{ .compatible = "renesas,etheravb-rcar-gen3" },
 	{ .compatible = "renesas,etheravb-rcar-gen4" },
+	{ .compatible = "renesas,etheravb-r9a07g044l" },
 	{ }
 };
 
