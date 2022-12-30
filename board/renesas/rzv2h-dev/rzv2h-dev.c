3c490415d690c58b5f29d4d822936ff35e3e7cf0
Nhan Nguyen <nhan.nguyen.yb@renesas.com>
2022-12-30 15:41:15 +0700

    board: renesas: rzv2h-dev: add board files
    Initialized board files for RZ/V2H.
    
    Signed-off-by: Nhan Nguyen <nhan.nguyen.yb@renesas.com>

---
+++ board/renesas/rzv2h-dev/rzv2h-dev.c
@@ -0,0 +1,47 @@
+#include <common.h>
+#include <cpu_func.h>
+#include <image.h>
+#include <init.h>
+#include <malloc.h>
+#include <netdev.h>
+#include <dm.h>
+#include <dm/platform_data/serial_sh.h>
+#include <asm/processor.h>
+#include <asm/mach-types.h>
+#include <asm/io.h>
+#include <linux/bitops.h>
+#include <linux/errno.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/gpio.h>
+#include <asm/arch/gpio.h>
+#include <asm/arch/rmobile.h>
+#include <asm/arch/rcar-mstp.h>
+#include <asm/arch/sh_sdhi.h>
+#include <i2c.h>
+#include <mmc.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+void s_init(void)
+{
+
+}
+
+int board_early_init_f(void)
+{
+
+	return 0;
+}
+
+int board_init(void)
+{
+	/* adress of boot parameters */
+	gd->bd->bi_boot_params = CONFIG_SYS_TEXT_BASE + 0x50000;
+
+	return 0;
+}
+
+void reset_cpu(void)
+{
+
+}
