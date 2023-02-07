5502146d1862d42baf8d44722c43ecd83c438795
Shinji Hirai <shinji.hirai.zn@renesas.com>
2023-02-07 14:24:33 +0900

    rzv2h-dev : SD2 Card Slot support

--- board/renesas/rzv2h-dev/rzv2h-dev.c
+++ board/renesas/rzv2h-dev/rzv2h-dev.c
@@ -22,9 +22,19 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
+#define PFC_BASE			0x10410000
+
+#define	P_2A				(PFC_BASE + 0x002A)
+#define	PM_2A				(PFC_BASE + 0x0154)
+#define	PMC_2A				(PFC_BASE + 0x022A)
+
 void s_init(void)
 {
-
+#if CONFIG_TARGET_RZV2H_DEV
+	*(volatile u8 *)PMC_2A   &= ~(0x03<<4);	/* PA5,PA4 port	*/
+	*(volatile u8 *)P_2A      = (*(volatile u32 *)P_2A  & ~(0x03<<4)) | (0x01 <<5); /* PA5=1,PA4=0		*/
+	*(volatile u16 *)PM_2A    = (*(volatile u32 *)PM_2A & ~(0x0f<<8)) | (0x0c <<8); /* PA5,PA4 output	*/
+#endif
 }
 
 int board_early_init_f(void)
