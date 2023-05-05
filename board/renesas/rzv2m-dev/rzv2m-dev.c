dc84b13cc88655e7103a0c4dec637704156f9e0c
Tai Huynh <tai.huynh.cp@renesas.com>
2023-05-05 16:42:52 +0700

    added: evk: files: rzv2m-dev
    Add definition of RZV2M EVK board
    
    Signed-off-by: Tai Huynh <tai.huynh.cp@renesas.com>

--- board/renesas/rzv2m-dev/rzv2m-dev.c
+++ board/renesas/rzv2m-dev/rzv2m-dev.c
@@ -18,19 +18,17 @@
 * you agree to the additional terms and conditions found by accessing the
 * following link:
 * http://www.renesas.com/disclaimer
-* Copyright (C) 2021 Renesas Electronics Corporation. All rights reserved.
-*******************************************************************************/ 
+* Copyright (C) 2022 Renesas Electronics Corporation. All rights reserved.
+ *******************************************************************************/ 
 #include <common.h>
 #include <malloc.h>
 #include <fdtdec.h>
 #include <linux/errno.h>
 #include <asm/system.h>
-#include "rdk_common.h"
-#include "rdk_pfc.h"
-#include "rdk_cmn_cpg.h"
-#include "rdk_usbtest.h"
-#include "rdk_sys.h"
-#include "rdk_psc.h"
+#include "evk_common.h"
+#include "evk_cmn_cpg.h"
+#include "evk_sys.h"
+#include "evk_psc.h"
 #include <init.h>
 
 /*
@@ -42,15 +40,19 @@ TARGET_BANK0_UNIT_LINUX settings as follows:
                BA_WE10 | BA10  //USB PERI
                BA_WE12 | BA12  //PCI
 */
-#define TARGET_BANK0_UNIT_LINUX        (BA_WE00 | BA00 | \
-                                                                       BA_WE02 | BA02 | \
-                                                                       BA_WE04 | BA04 | \
-                                                                       BA_WE08 | BA08 | \
-                                                                       BA_WE10 | BA10 | \
-                                                                       BA_WE12 | BA12)
+#define TARGET_BANK0_UNIT_LINUX  (BA_WE00 | BA00 | \
+                                  BA_WE02 | BA02 | \
+                                  BA_WE04 | BA04 | \
+                                  BA_WE08 | BA08 | \
+                                  BA_WE10 | BA10 | \
+                                  BA_WE12 | BA12)
 
-
-#define TARGET_BANK1_UNIT_LINUX        (BA_WE00 | BA00)       //ETH0 BANK
+/*
+       TARGET_BANK0_UNIT_LINUX settings as follows:
+               BA_WE00 | BA00  //ETH0
+*/
+#define TARGET_BANK1_UNIT_LINUX  (BA_WE00 | BA00)
+	
 /*
        TARGET_DRP_UNIT_LINUX settings as follows:
                BA_WE00 | BA00  //DRPA M0
@@ -59,41 +61,17 @@ TARGET_BANK0_UNIT_LINUX settings as follows:
                BA_WE06 | BA06  //DRPA M3
 */
 #define TARGET_DRP_UNIT_LINUX  (BA_WE00 | BA00 | \
-                                                                       BA_WE02 | BA02 | \
-                                                                       BA_WE04 | BA04 | \
-                                                                       BA_WE06 | BA06)
-
+                                BA_WE02 | BA02 | \
+                                BA_WE04 | BA04 | \
+                                BA_WE06 | BA06)
 
 DECLARE_GLOBAL_DATA_PTR;
 
-static void rdk_pfc_init(void)
-{
-	do
-	{
-		PFC_SetPinFunc(PFC_SELECT_URT0);
-		PFC_SetPinFunc(PFC_SELECT_URT1);
-		PFC_SetPinFunc(PFC_SELECT_EMM);
-		PFC_SetPinFunc(PFC_SELECT_SDI0);
-		PFC_SetPinFunc(PFC_SELECT_ETH0);
-		PFC_WriteReg(PFC_P17_GPIO_DO, 0x10001);
-		CMN_DelayInUS(50);
-		PFC_SetPinFunc(PFC_SELECT_IIC00);
-		PFC_SetPinFunc(PFC_SELECT_PWM0_7);
-		PFC_SetPinFunc(PFC_SELECT_PWM8_15);
-		PFC_SetPinFunc(PFC_SELECT_CSI2);
-		PFC_SetPinFunc(PFC_SELECT_CSI3);
-		PFC_SetPinFunc(PFC_SELECT_CSI4);
-		PFC_SetPinFunc(PFC_SELECT_CSI5);
-	}
-	while (0);
-	return;
-}
-
-static void drp_init(void){
+static void drp_a_init(void){
 
 	uint32_t regval;
 	
-    //Initialize the PLL6
+    	//Initialize the PLL6
 	regval = CMN_REG_Read32(0xa350003c);
 	while((regval & 0x11) != 0x00){
 		regval = CMN_REG_Read32(0xa350003c);
@@ -110,7 +88,7 @@ static void drp_init(void){
 		CMN_DelayInUS(1);
 	}
 
-    //Power on with PD_DRPA
+    	//Power on with PD_DRPA
 	CMN_REG_Write32(0xA3600018, 0x0BB70BB7);
 	CMN_REG_Write32(0xa3600000, 0x00000015);
 	regval = CMN_REG_Read32(0xa3600044);
@@ -126,16 +104,16 @@ static void drp_init(void){
 		CMN_DelayInUS(1);
 	}
 
-    //Clear interrupt status with PMC
+    	//Clear interrupt status with PMC
 	CMN_REG_Write32(0xa3600048, 0x00000001);
 
-    //Clock supply with PD_DRP
+    	//Clock supply with PD_DRP
 	CMN_REG_Write32(0xa350044c,0x00040004);
 	CMN_DelayInUS(1000);
 	CMN_REG_Write32(0xa3500450,0x00040004);
 	CMN_DelayInUS(1000);
 
-    //Isolation off with DRP
+    	//Isolation off with DRP
 	CMN_REG_Write32(0xa360002c, 0x00000000);
 	regval = CMN_REG_Read32(0xa360002c);
 	while((regval & 0x8000) != 0x8000){
@@ -162,239 +140,242 @@ static void drp_init(void){
 
 }
 
-static void rdk_cpg_init(void)
+static void pcie_init (void)
+{
+	// set SYS register
+	CMN_REG_Write32(0xA3F03094,0x00000000);
+	CMN_REG_Write32(0xA3F03090,0x00000001);
+	CMN_DelayInUS(10);
+
+	/* assert RESET */
+	CPG_SetResetCtrl(3, 0x1000, 0x0000);
+	CMN_DelayInUS(2);
+
+	/* disable supply clock */
+	CPG_SetClockCtrl(4, 0x0007, 0x0000);
+	CMN_DelayInUS(2);
+
+	/* deassert RESET */
+	CPG_SetResetCtrl(3, 0x1000, 0x1000);
+	CMN_DelayInUS(2);
+
+	/* enable supply clock */
+	CPG_SetClockCtrl(4, 0x0007, 0x0007);
+	CMN_DelayInUS(2);
+}
+
+static void div_init(void)
+{
+	int t;
+
+	/* select  DEVB(400MHz) ,select DEVD(200MHz) and select DEVE(100MHz) */
+	CPG_WriteReg(CPG_SYS_DDIV, (CPG_SYS_DDIV_WEN_DIVB|CPG_SYS_DDIV_WEN_DIVE|CPG_SYS_DDIV_WEN_DIVD));
+	t = 0;
+	while (1000000 > t++)
+	{
+		if (0 == (CPG_ReadReg(CPG_CLKSTATUS) & CPG_CLKSTATUS_DIVX))
+		{
+		break;	
+		}
+		CMN_DelayInUS(10);
+	}
+}
+
+static void sdi_emm_init(void)
+{
+	/* select SDI0 EMM clock source(200MHz) */
+	CPG_WriteReg(CPG_SDIEMM_SSEL, (CPG_SDIEMM_SSEL_SELSDI|CPG_SDIEMM_SSEL_WEN_SELSDI));
+	/* enable supply clock */
+	CPG_SetClockCtrl(3, 0x0f0f, 0x0f0f);
+	CMN_DelayInUS(1);
+	/* asset RESET */
+	CPG_SetResetCtrl(3, 0x0005, 0x0000);
+	CPG_WaitResetMon(0, CPG_RST_MON_SDI0, CPG_RST_MON_SDI0);
+	CPG_WaitResetMon(0, CPG_RST_MON_EMM,  CPG_RST_MON_EMM);
+	/* deasset RESET */
+	CPG_SetResetCtrl(3, 0x0005, 0x0005);
+	CPG_WaitResetMon(0, CPG_RST_MON_SDI0, 0);
+	CPG_WaitResetMon(0, CPG_RST_MON_EMM,  0);
+}
+
+static void usb_init(void)
+{
+	/* usb reset assart */
+	CPG_SetResetCtrl(3, 0x0780, 0x0000);
+	CMN_DelayInUS(1);//50ns
+	/* usb clock off */
+	CPG_SetClockCtrl(4, 0x0070, 0x0000);
+	CMN_DelayInUS(1);//125ns
+
+	/* usb reset assart */
+	CPG_SetResetCtrl(3, 0x0780, 0x0780);
+	CMN_DelayInUS(1);//50ns
+
+	/* usb clock on */
+	CPG_SetClockCtrl(4, 0x0070, 0x0070);
+	CMN_DelayInUS(1);//125ns
+}
+
+static void iic_init(void)
+{
+	/* asset RESET */
+	/* IIC_GPA_PRESETN:IIC0,IIC1 */
+	/* IIC_GPB_PRESETN:IIC2,IIC3 */
+	CPG_SetResetCtrl(6, 0x0300, 0x0000);
+	CMN_DelayInUS(1);
+
+	/* disable supply clock */
+	/* IIC_PCLK[0]:IIC0,IIC1 */
+	/* IIC_PCLK[1]:IIC2,IIC3 */
+	CPG_SetClockCtrl(9, 0x1000, 0x0000);
+	CPG_SetClockCtrl(10, 0x1000, 0x0000);
+	CMN_DelayInUS(1);
+
+	/* deasset RESET */
+	/* IIC_GPA_PRESETN:IIC0,IIC1 */
+	/* IIC_GPB_PRESETN:IIC2,IIC3 */
+	CPG_SetResetCtrl(6, 0x0300, 0x0300);
+	CMN_DelayInUS(1);
+
+	/* enable supply clock */
+	/* IIC_PCLK[0]:IIC0,IIC1 */
+	/* IIC_PCLK[1]:IIC2,IIC3 */
+	CPG_SetClockCtrl(9, 0x1000, 0x1000);
+	CPG_SetClockCtrl(10, 0x1000, 0x1000);
+	CMN_DelayInUS(1);
+}
+
+static void ether_init(void)
+{
+	/* eth0 clock on */
+	CPG_SetClockCtrl(4, 0x0300, 0x0300);
+	CPG_SetClockCtrl(16, 0x2000, 0x2000);
+	CMN_DelayInUS(1);
+
+	/* eth0 reset assart */
+	CPG_SetResetCtrl(3, 0x0800, 0x0000);
+	CPG_WaitResetMon(0, CPG_RST_MON_ETH0, CPG_RST_MON_ETH0);
+
+	/* eth0 reset deassert */
+	CPG_SetResetCtrl(3, 0x0800, 0x0800);
+	CPG_WaitResetMon(0, CPG_RST_MON_ETH0, 0);
+}
+
+static void pwm_init(void)
+{
+	/* enable supply clock */
+	/* PWM[14]:b'10 - PWM[8]:b'4, GRPF_PCLK:b'0*/
+	CPG_SetClockCtrl(14, 0x07F1, 0x07F1);
+	CMN_DelayInUS(1);
+	
+	/* PWM8-15 reset assart with PWM_GPF_PRESETN */
+	CPG_SetResetCtrl(6, 0x0020, 0x0000);
+	CPG_WaitResetMon(0, CPG_RST_MON_PWM_1, CPG_RST_MON_PWM_1);
+	
+	/* PWM8-15 reset deassart with PWM_GPF_PRESETN */
+	CPG_SetResetCtrl(6, 0x0020, 0x0020);
+	CPG_WaitResetMon(0, CPG_RST_MON_PWM_1, 0);
+}
+
+static void csi_init(void)
+{
+	/* enable supply clock */
+	/* CSI[4]:b'12, CPERI_GRPH_PCLK:b'1*/
+	CPG_SetClockCtrl(15, 0x1002, 0x1002);
+	CMN_DelayInUS(1);
+
+	/* CSI4 reset assart with CSI_GPH_PRESETN */
+	CPG_SetResetCtrl(6, 0x0080, 0x0000);
+	CPG_WaitResetMon(0, CPG_RST_MON_CSI_2, CPG_RST_MON_CSI_2);
+
+	/* CSI4 reset deassert */
+	CPG_SetResetCtrl(6, 0x0080, 0x0080);
+	CPG_WaitResetMon(0, CPG_RST_MON_CSI_2, 0);
+}
+
+static void tim_init(void)
+{
+	/* TIM group B and C reset assart */
+	CPG_SetResetCtrl(6, 0x0006, 0x0000);
+	CMN_DelayInUS(1);
+	
+	/* TIM group B and TIM 8-15 clock off */
+	CPG_SetClockCtrl(10, 0x0FF1, 0x0000);
+	/* TIM group C and TIM 16-23 clock off */
+	CPG_SetClockCtrl(11, 0x0FF1, 0x0000);
+	CMN_DelayInUS(1);
+
+	/* TIM group B and C reset deassert */
+	CPG_SetResetCtrl(6, 0x0006, 0x0006);
+	CMN_DelayInUS(1);
+
+	/* TIM group B and TIM 8-15 clock on */
+	CPG_SetClockCtrl(10, 0x0FF1, 0x0FF1);
+	/* TIM group C and TIM 16-23 clock on */
+	CPG_SetClockCtrl(11, 0x0FF1, 0x0FF1);
+	CMN_DelayInUS(1);
+}
+
+static void evk_cpg_init(void)
 {
 	int t;
 	do
-	{	
-		/* select  DEVB(400MHz) ,select DEVD(200MHz) and select DEVE(100MHz) */
-		CPG_WriteReg(CPG_SYS_DDIV, (CPG_SYS_DDIV_WEN_DIVB|CPG_SYS_DDIV_WEN_DIVE|CPG_SYS_DDIV_WEN_DIVD));
-		t = 0;
-		while (1000000 > t++)
-		{
-			if (0 == (CPG_ReadReg(CPG_CLKSTATUS) & CPG_CLKSTATUS_DIVX))
-			{
-				break;
-			}
-			CMN_DelayInUS(10);
-		}
+	{
+		/**********************************/
+		/*    Clock division control      */
+		/**********************************/
+		div_init();		
 
 		/**********************************/
-		/**** URT *******************/
+		/*    SDI0 SDI1 EMM               */
 		/**********************************/
-		/* select URT clock source(48MHz) */
-		//CPG_WriteReg(CPG_URT_RCLK_SSEL, CPG_URT_RCLK_SSEL_WEN_SELW0 | CPG_URT_RCLK_SSEL_WEN_SELW1);
-
-		/* enable supply clock to URT */
-		CPG_SetClockCtrl(15, 0x0070, 0x0070);
-		CMN_DelayInUS(1);
-
-		/* asset RESET to URT0,1 */
-		CPG_SetResetCtrl(6, 0x0400, 0x0000);
-		CPG_WaitResetMon(/*500000*/0, CPG_RST_MON_URT, CPG_RST_MON_URT);
-
-		/* deasset RESET to URT0,1 */
-		CPG_SetResetCtrl(6, 0x0400, 0x0400);
-		CPG_WaitResetMon(/*500000*/0, CPG_RST_MON_URT, 0);
+		sdi_emm_init();
 
 		/**********************************/
-		/**** SDI0 EMM ************/
+		/*    USB                         */
 		/**********************************/
-		/* select SDI0 EMM clock source(200MHz) */
-		CPG_WriteReg(CPG_SDIEMM_SSEL, (CPG_SDIEMM_SSEL_SELSDI|CPG_SDIEMM_SSEL_WEN_SELSDI));
-
-		/* enable supply clock */
-		CPG_SetClockCtrl(3, 0x0f0f, 0x0f0f);
-		CMN_DelayInUS(1);
-
-		/* asset RESET */
-		CPG_SetResetCtrl(3, 0x0005, 0x0000);
-		CPG_WaitResetMon(0, CPG_RST_MON_SDI0, CPG_RST_MON_SDI0);
-		CPG_WaitResetMon(0, CPG_RST_MON_EMM,  CPG_RST_MON_EMM);
-
-		/* deasset RESET */
-		CPG_SetResetCtrl(3, 0x0005, 0x0005);
-		CPG_WaitResetMon(0, CPG_RST_MON_SDI0, 0);
-		CPG_WaitResetMon(0, CPG_RST_MON_EMM,  0);
+		usb_init();
 
 		/**********************************/
-		/**** USB *******************/
+		/*    IIC0,2                      */
 		/**********************************/
-		/* select USB DEVD(200MHz) and select USB DEVE(100MHz) */
-		//CPG_WriteReg(CPG_SYS_DDIV, (CPG_SYS_DDIV_WEN_DIVE|CPG_SYS_DDIV_WEN_DIVD));	
-		// while (true)
-		// {
-		// 	if (0 == (CPG_ReadReg(CPG_CLKSTATUS) & CPG_CLKSTATUS_DIVX))
-		// 	{
-		// 		break;
-		// 	}
-		// 	CMN_DelayInUS(10);
-		// }
-
-		/* usb reset assart */
-		CPG_SetResetCtrl(3, 0x0780, 0x0000);
-		CMN_DelayInUS(1);//50ns
-
-		/* usb clock off */
-		CPG_SetClockCtrl(4, 0x0070, 0x0000);
-		CMN_DelayInUS(1);//125ns
-
-		/* usb reset assart */
-		CPG_SetResetCtrl(3, 0x0780, 0x0780);
-		CMN_DelayInUS(1);//50ns
-
-		/* usb clock on */
-		CPG_SetClockCtrl(4, 0x0070, 0x0070);
-		CMN_DelayInUS(1);//125ns
-
-		/**********************************/
-		/**** IIC0-3****************/
-		/**********************************/
-		/* asset RESET */
-		CPG_SetResetCtrl(6, 0x0300, 0x0000);
-		CMN_DelayInUS(1);
-
-		/* disable supply clock */
-		CPG_SetClockCtrl(9, 0x1000, 0x0000);
-		CPG_SetClockCtrl(10, 0x1000, 0x0000);
-		CMN_DelayInUS(1);
-
-		/* deasset RESET */
-		CPG_SetResetCtrl(6, 0x0300, 0x0300);
-		CMN_DelayInUS(1);
-
-		/* enable supply clock */
-		CPG_SetClockCtrl(9, 0x1000, 0x1000);
-		CPG_SetClockCtrl(10, 0x1000, 0x1000);
-		CMN_DelayInUS(1);
-
-		/**********************************/
-		/**** ETH0 ****************/
-		/**********************************/
-
-		/* eth0 clock on */
-		CPG_SetClockCtrl(4, 0x0300, 0x0300);
-		CPG_SetClockCtrl(16, 0x2000, 0x2000);
-		CMN_DelayInUS(1);
-
-		/* eth0 reset assart */
-		CPG_SetResetCtrl(3, 0x0800, 0x0000);
-		CPG_WaitResetMon(0, CPG_RST_MON_ETH0, CPG_RST_MON_ETH0);
-
-		/* eth0 reset deassert */
-		CPG_SetResetCtrl(3, 0x0800, 0x0800);
-		CPG_WaitResetMon(0, CPG_RST_MON_ETH0, 0);
-
-		/**********************************/
-		/**** PWM0-15 ****************/
-		/**********************************/
-		CPG_SetClockCtrl(13, 0x0FF1, 0x0FF1);
-		CPG_SetClockCtrl(14, 0x0FF1, 0x0FF1);
-		CMN_DelayInUS(1);
+		iic_init();
 		
-		/* PWM0-7 reset assart */
-		CPG_SetResetCtrl(6, 0x0030, 0x0000);
-		CPG_WaitResetMon(0, CPG_RST_MON_PWM_0 | CPG_RST_MON_PWM_1, CPG_RST_MON_PWM_0 | CPG_RST_MON_PWM_1);
-		
-		/* PWM0-7 reset deassert */
-		CPG_SetResetCtrl(6, 0x0030, 0x0030);
-		CPG_WaitResetMon(0, CPG_RST_MON_PWM_0 | CPG_RST_MON_PWM_1, 0);
-
 		/**********************************/
-		/**** CSI2-5 ****************/
+		/*    ETH0                        */
 		/**********************************/
-		/* select CSI REF */
-		//Keep defalt setting
-		//CPG_WriteReg(CPG_CSI_RCLK_SSEL, (CPG_CSI_RCLK_SSEL_WEN_SELCSI2|CPG_CSI_RCLK_SSEL_WEN_SELCSI3|CPG_CSI_RCLK_SSEL_WEN_SELCSI4|CPG_CSI_RCLK_SSEL_WEN_SELCSI5));
-
-		CPG_SetClockCtrl(15, 0x3C03, 0x3C03);
-		CMN_DelayInUS(1);
-		
-		/* CSI4 reset assart */
-		CPG_SetResetCtrl(6, 0x00C0, 0x0000);
-		CPG_WaitResetMon(0, CPG_RST_MON_CSI_1|CPG_RST_MON_CSI_2, CPG_RST_MON_CSI_1|CPG_RST_MON_CSI_2);
-		
-		/* CSI4 reset deassert */
-		CPG_SetResetCtrl(6, 0x00C0, 0x00C0);
-		CPG_WaitResetMon(0, CPG_RST_MON_CSI_1|CPG_RST_MON_CSI_2, 0);
-
+		ether_init();
 	    
-		CPG_SetClockCtrl(4, 0x0007, 0x0007);
-		CMN_DelayInUS(1);
-		CPG_SetResetCtrl(3, 0x1000, 0x0000);
-		CMN_DelayInUS(1);
-		CPG_SetResetCtrl(3, 0x1000, 0x1000);
-		CMN_DelayInUS(1);
-		
 		/**********************************/
-		/**** TIM0-31 ****************/
+		/*    PWM8-14                     */
 		/**********************************/
-		/* TIM group A-D reset assart */
-		CPG_SetResetCtrl(6, 0x000F, 0x0000);
-		CMN_DelayInUS(1);
-		
-		/* TIM group A and TIM 0-7 clock off */
-		CPG_SetClockCtrl(9, 0x0FF1, 0x0000);
-		/* TIM group B and TIM 8-15 clock off */
-		CPG_SetClockCtrl(10, 0x0FF1, 0x0000);
-		/* TIM group C and TIM 16-23 clock off */
-		CPG_SetClockCtrl(11, 0x0FF1, 0x0000);
-		/* TIM group D and TIM 24-31 clock off */
-		CPG_SetClockCtrl(12, 0x0FF1, 0x0000);
-		CMN_DelayInUS(1);
+		pwm_init();
 
-		/* TIM group A-D reset deassert */
-		CPG_SetResetCtrl(6, 0x000F, 0x000F);
-		CMN_DelayInUS(1);
-
-		/* TIM group A and TIM 0-7 clock on */
-		CPG_SetClockCtrl(9, 0x0FF1, 0x0FF1);
-		/* TIM group B and TIM 8-15 clock on */
-		CPG_SetClockCtrl(10, 0x0FF1, 0x0FF1);
-		/* TIM group C and TIM 16-23 clock on */
-		CPG_SetClockCtrl(11, 0x0FF1, 0x0FF1);
-		/* TIM group D and TIM 24-31 clock on */
-		CPG_SetClockCtrl(12,0x0FF1, 0x0FF1);
-		CMN_DelayInUS(1);
-		
 		/**********************************/
-		/**** DMAA ****************/
+		/*    CSI4                        */
 		/**********************************/
+		csi_init();
 
-		/* DMAA clock on */ 
-		CPG_SetClockCtrl(1, 0x0800, 0x0800);
+		/**********************************/
+		/*    TIM8-23                     */
+		/**********************************/
+		tim_init();
 			
-		/* DMAA reset assart */
-		CPG_SetResetCtrl(1, 0x0080, 0x0000);
-		CPG_WaitResetMon(0, CPG_RST_MON_DMAA,CPG_RST_MON_DMAA);
+		/**********************************/
+		/*    DRPA                        */
+		/**********************************/
+		drp_a_init();
 		
-		/* DMAA reset deassert */
-		CPG_SetResetCtrl(1, 0x0080, 0x0080);
-		CPG_WaitResetMon(0, CPG_RST_MON_DMAA,0);
-
-
 		/**********************************/
-		/**** DRPA ****************/
+		/*    PCIe                         */
+ 		/**********************************/
 		/**********************************/
-		drp_init();
+		pcie_init();
 	}
 	while (0);
 	return;
 }
 
-static void rdk_module_init(void)
-{
-	//USB TEST Module initialize
-	CMN_REG_Write32(USBTEST_RESET2,  0x00000001);
-	CMN_DelayInUS(10);
-	CMN_REG_Write32(USBTEST_CLKRST2, 0x00000005);
-	CMN_REG_Write32(USBTEST_CLKRST2, 0x00000004);
-	CMN_DelayInUS(20);
-	CMN_REG_Write32(USBTEST_RESET1,  0x00000300);
-	CMN_REG_Write32(USBTEST_RESET1,  0x00000000);
-	CMN_DelayInUS(75);
-
-}
 int32_t ilStartUpUART(void)
 {
 	return CMN_SUCCESS;
@@ -404,7 +385,7 @@ extern void _start(void);
 
 void s_init(void)
 {
-
+	/* change the EL2 with ca53 core0 */
 	if(3 == current_el()){
 
 		armv8_switch_to_el2((u64)NULL, 0, 0, 0,
@@ -417,18 +398,18 @@ void s_init(void)
 
 int board_early_init_f(void)
 {
-    
-	rdk_pfc_init();
-    
 	/* move to active for SRAM in ETH0 */
 	SYS_MoveToActiveSRAM(SYS_SRAM_ETH0);
-    
-	rdk_cpg_init();
 
-	rdk_module_init();
-    
-	rdk_psc_init();
+	evk_cpg_init();
+	evk_psc_init();
 	
+	/*- Release Ether PHY reset : PFC_P17_GPIO_DO -*/
+	CMN_REG_Write32(0xB6250440, 0x10001);
+	
+	/* HDMI phy setting : SYS GEN_REG2 reg : offset 208h */
+	CMN_REG_Write32(0xA3F03208,0x60106010);
+
 	return 0;
 }
 
@@ -436,9 +417,6 @@ int board_init(void)
 {
 	/* adress of boot parameters */
 	gd->bd->bi_boot_params = CONFIG_SYS_TEXT_BASE + 0x50000;
-
-	/* init UART */
-	//serial_init();
 
 	return 0;
 }
