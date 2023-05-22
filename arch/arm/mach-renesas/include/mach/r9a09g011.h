3dbfa24eabf3290533186b8e5820def8324157ba
Tai Huynh <tai.huynh.cp@renesas.com>
2023-05-22 18:37:44 +0700

    arch: arm: mach-rmobile: include: mach: r9a09g011.h: r9a09g055ma3gbg.h: Add RZ/V2[M,MA] header files.
    Add header files in mach-rmobile for RZ/V2[M,MA].
    
    Signed-off-by: Tai Huynh <tai.huynh.cp@renesas.com>

---
+++ arch/arm/mach-renesas/include/mach/r9a09g011.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+   + * Copyright (C) 2012 Renesas Solutions Corp.
+   + */
+
+#ifndef __ASM_ARCH_R9A09G011_H
+#define __ASM_ARCH_R9A09G011_H
+
+/*
+   + * R9A09G011 I/O Addresses
+   + */
+
+
+#ifndef __ASSEMBLY__
+#include <asm/types.h>
+
+#endif
+
+#endif /* __ASM_ARCH_R9A09G011_H */
