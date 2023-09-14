45789ef07f00f641d707d16a4cbf887a445caabc
hienhuynh <hien.huynh.px@renesas.com>
2023-09-14 15:45:38 +0700

    hihope_rzg2/silinux_ek874_defconfig/rcar-gen3-common: update default environment
    Updates default environment for RZ/G2HMNE Series.
    Default environment will boot up Linux from:
    - uSD if it is available.
    - eMMC it uSD does not exist.
    Also add variables to check SoC ID and Board revision to get the
    correct dtb files to boot up.
    
    Signed-off-by: hienhuynh <hien.huynh.px@renesas.com>

--- include/configs/rcar-gen3-common.h
+++ include/configs/rcar-gen3-common.h
@@ -60,14 +60,54 @@
 
 /* ENV setting */
 
+#if defined(CONFIG_TARGET_SILINUX_EK874)
+#define CONFIG_EXTRA_ENV_SETTINGS	\
+	"usb_pgood_delay=2000\0" \
+	"bootm_size=0x10000000\0" \
+	"prodsdbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk0p2 \0" \
+	"prodemmcbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk1p2 \0" \
+	"bootimage=unzip 0x4A080000 0x48080000; booti 0x48080000 - 0x48000000 \0" \
+	"emmcload=ext4load mmc 1:2 0x48080000 boot/Image;ext4load mmc 1:2 0x48000000 boot/${dtb_name};run prodemmcbootargs \0" \
+	"sdload=ext4load mmc 0:2 0x48080000 boot/Image;ext4load mmc 0:2 0x48000000 boot/${dtb_name};run prodsdbootargs \0" \
+	"bootcmd_check=if mmc dev 0; then run sdload; else run emmcload; fi \0" \
+	"dtb_check=if test ${board_rev} = E;then setenv dtb_name r8a774c0-ek874.dtb;else setenv dtb_name r8a774c0-ek874-revc.dtb;fi \0"
+#elif defined(CONFIG_TARGET_HIHOPE_RZG2)
+#define CONFIG_EXTRA_ENV_SETTINGS	\
+	"usb_pgood_delay=2000\0" \
+	"bootm_size=0x10000000\0" \
+	"prodsdbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk1p2 \0" \
+	"prodemmcbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk0p2 \0" \
+	"bootimage=unzip 0x4A080000 0x48080000; booti 0x48080000 - 0x48000000 \0" \
+	"emmcload=ext4load mmc 1:2 0x48080000 boot/Image;ext4load mmc 1:2 0x48000000 boot/${dtb_name};run prodemmcbootargs \0" \
+	"sdload=ext4load mmc 0:2 0x48080000 boot/Image;ext4load mmc 0:2 0x48000000 boot/${dtb_name};run prodsdbootargs \0" \
+	"bootcmd_check=if mmc dev 0; then run sdload; else run emmcload; fi \0" \
+	"g2m_v1_dtb_check=if test ${soc_id} = r8a774a1;then if test ${board_rev} = 2;then setenv dtb_name r8a774a1-hihope-rzg2m-rev2-ex.dtb;else setenv dtb_name r8a774a1-hihope-rzg2m-ex.dtb;fi;fi \0" \
+	"g2m_v3_dtb_check=if test ${soc_id} = r8a774a3;then setenv dtb_name r8a774a3-hihope-rzg2m-ex.dtb;fi\0" \
+	"g2n_dtb_check=if test ${soc_id} = r8a774b1;then if test ${board_rev} = 2;then setenv dtb_name r8a774b1-hihope-rzg2n-rev2-ex.dtb;else setenv dtb_name r8a774b1-hihope-rzg2n-ex.dtb;fi;fi \0" \
+	"g2h_dtb_check=if test ${soc_id} = r8a774e1;then setenv dtb_name r8a774e1-hihope-rzg2h-ex.dtb;fi \0"
+#else
 #define CONFIG_EXTRA_ENV_SETTINGS	\
 	"usb_pgood_delay=2000\0" \
 	"bootm_size=0x10000000\0"
+#endif
 
+#if defined(CONFIG_TARGET_SILINUX_EK874)
 #define CONFIG_BOOTCOMMAND	\
+	"board_rev=${board_rev}; env default -a; " \
+	"run dtb_check; run bootcmd_check; run bootimage"
+#elif defined(CONFIG_TARGET_HIHOPE_RZG2)
+#define CONFIG_BOOTCOMMAND	\
+	"soc_id=${platform}; board_rev=${board_rev}; env default -a; " \
+	"run g2m_v1_dtb_check; run g2m_v3_dtb_check; " \
+	"run g2n_dtb_check; run g2h_dtb_check; " \
+	"run bootcmd_check; run bootimage"
+#else
+#define CONFIG_BOOTCOMMAND     \
 	"tftp 0x48080000 Image; " \
 	"tftp 0x48000000 Image-"CONFIG_DEFAULT_FDT_FILE"; " \
 	"booti 0x48080000 - 0x48000000"
+#endif
+
 
 /* SPL support */
 #if defined(CONFIG_R8A7795) || defined(CONFIG_R8A7796) || defined(CONFIG_R8A77965)
