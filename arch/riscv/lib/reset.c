36a061e0618654e82365256c62cc3f932e9afcd6
Tranh Ha <tranh.ha.xb@renesas.com>
2023-10-02 10:12:13 +0700

    riscv: Add reset command to support for rzfive
    This commit to support reset command for rzfive in u-boot.
    
    Signed-off-by: Tranh Ha <tranh.ha.xb@renesas.com>

--- arch/riscv/lib/reset.c
+++ arch/riscv/lib/reset.c
@@ -5,14 +5,21 @@
 
 #include <common.h>
 #include <command.h>
-#include <hang.h>
+#include <cpu_func.h>
+#include <linux/delay.h>
 
 int do_reset(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 {
 	printf("resetting ...\n");
 
+#ifdef CONFIG_ARCH_RZMPU
+	mdelay(50);                             /* wait 50 ms */
+
+	reset_cpu();
+#else
 	printf("reset not supported yet\n");
 	hang();
+#endif
 
 	return 0;
 }
