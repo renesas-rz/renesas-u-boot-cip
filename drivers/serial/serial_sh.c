4c290fe2a63352ab78f3d2137e31263a653951c2
Linh Hua <linh.hua.jc@renesas.com>
2023-10-25 15:55:39 +0700

    serial: serial_sh: ignore HSCIF for RZ/G2L
    Signed-off-by: Linh Hua <linh.hua.jc@renesas.com>

--- drivers/serial/serial_sh.c
+++ drivers/serial/serial_sh.c
@@ -58,8 +58,10 @@ static void sh_serial_init_generic(struct uart_port *port)
 	sci_out(port, SCSPTR, 0x0003);
 #endif
 
+#if !defined(CONFIG_R9A07G044L)
 	if (port->type == PORT_HSCIF)
 		sci_out(port, HSSRR, HSSRR_SRE | HSSRR_SRCYC8);
+#endif
 }
 
 static void
