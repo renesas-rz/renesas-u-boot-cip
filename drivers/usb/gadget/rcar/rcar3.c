f72910bb690b4f72b4dee4acb117782018e34e65
Huy Nguyen <huy.nguyen.wh@renesas.com>
2023-12-05 10:05:35 +0700

    usb: gadget: rcar: add support for RZ/G2E
    This patch adds support for RZ/G2E because it is as same as RCAR D3.
    
    This SoC needs to release the PLL reset by the UGCTRL register.
    So, since this is not the same as other R-Car Gen3 SoCs,
    this patch adds a new type as "USBHS_TYPE_RCAR_GEN3_WITH_PLL".
    
    Signed-off-by: Huy Nguyen <huy.nguyen.wh@renesas.com>

--- drivers/usb/gadget/rcar/rcar3.c
+++ drivers/usb/gadget/rcar/rcar3.c
@@ -15,20 +15,36 @@
 #include <asm/io.h>
 #include "common.h"
 #include "rcar3.h"
+#include <linux/delay.h>
 
 #define LPSTS		0x102
+#define UGCTRL		0x180   /* 32-bit register */
 #define UGCTRL2		0x184	/* 32-bit register */
+#define UGSTS		0x188   /* 32-bit register */
 
 /* Low Power Status register (LPSTS) */
 #define LPSTS_SUSPM	0x4000
 
+/* R-Car D3 only: USB General control register (UGCTRL) */
+#define UGCTRL_PLLRESET		0x00000001
+#define UGCTRL_CONNECT		0x00000004
+
 /* USB General control register 2 (UGCTRL2), bit[31:6] should be 0 */
 #define UGCTRL2_RESERVED_3	0x00000001	/* bit[3:0] should be B'0001 */
 #define UGCTRL2_USB0SEL_OTG	0x00000030
+#define UGCTRL2_VBUSSEL		0x00000400
+
+/* R-Car D3 only: USB General status register (UGSTS) */
+#define UGSTS_LOCK		0x00000100
 
 void usbhs_write32(struct usbhs_priv *priv, u32 reg, u32 data)
 {
 	writel(data, priv->base + reg);
+}
+
+static u32 usbhs_read32(struct usbhs_priv *priv, u32 reg)
+{
+	return readl(priv->base + reg);
 }
 
 static int usbhs_rcar3_power_ctrl(struct platform_device *pdev,
@@ -37,13 +53,32 @@ static int usbhs_rcar3_power_ctrl(struct platform_device *pdev,
 	struct usbhs_priv *priv = usbhs_pdev_to_priv(pdev);
 
 	pr_dbg("++%s\n", __func__);
+#if defined(CONFIG_R8A774C0)
+	u32 val;
+	int timeout = 1000;
 
+	if (enable) {
+		usbhs_write32(priv, UGCTRL, 0); /* release PLLRESET */
+		usbhs_write32(priv, UGCTRL2, UGCTRL2_RESERVED_3 | UGCTRL2_USB0SEL_OTG | UGCTRL2_VBUSSEL);
+		usbhs_bset(priv, LPSTS, LPSTS_SUSPM, LPSTS_SUSPM);
+		do {
+			val = usbhs_read32(priv, UGSTS);
+			udelay(1);
+		} while (!(val & UGSTS_LOCK) && timeout--);
+		usbhs_write32(priv, UGCTRL, UGCTRL_CONNECT);
+	} else {
+		usbhs_write32(priv, UGCTRL, 0); /* release PLLRESET */
+		usbhs_bset(priv, LPSTS, LPSTS_SUSPM, 0);
+		usbhs_write32(priv, UGCTRL, UGCTRL_PLLRESET);
+	}
+#else
 	usbhs_write32(priv, UGCTRL2, UGCTRL2_RESERVED_3 | UGCTRL2_USB0SEL_OTG);
 
 	if (enable)
 		usbhs_bset(priv, LPSTS, LPSTS_SUSPM, LPSTS_SUSPM);
 	else
 		usbhs_bset(priv, LPSTS, LPSTS_SUSPM, 0);
+#endif
 
 	pr_dbg("--%s\n", __func__);
 
