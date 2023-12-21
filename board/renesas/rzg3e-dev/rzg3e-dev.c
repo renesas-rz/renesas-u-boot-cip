6fcdb691315baae11dd4faca845d93b9aa30f31a
hienhuynh <hien.huynh.px@renesas.com>
2023-12-21 21:47:03 +0700

    board: renesas: rzg3e-dev: add pin configuration for SDHI
    Add pin configuration for all SDHI channels:
    - RZ/G3E Dev: QSD1 and QSD2.
    - RZ/G3E Smarc: QSD2.
    
    Signed-off-by: hienhuynh <hien.huynh.px@renesas.com>

--- board/renesas/rzg3e-dev/rzg3e-dev.c
+++ board/renesas/rzg3e-dev/rzg3e-dev.c
@@ -19,24 +19,45 @@
 #include <asm/arch/sh_sdhi.h>
 #include <i2c.h>
 #include <mmc.h>
+#include <dt-bindings/pinctrl/rzv2h-pinctrl.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
-#define PFC_BASE                       0x10410000
-#define        P_34                            (PFC_BASE + 0x0034)
-#define        PM_34                           (PFC_BASE + 0x0168)
-#define        PMC_34                          (PFC_BASE + 0x0234)
+#define PFC_BASE		0x10410000
 
+#define P(x)			(PFC_BASE + 0x20 + PORT_##x)
+#define PM(x)			(PFC_BASE + 0x140 + 2 * PORT_##x)
+#define PMC(x)			(PFC_BASE + 0x220 + PORT_##x)
+
+#define PFC_PWPR		(PFC_BASE + 0x3C04)
+#define PFC_PWPR_REGWE_A	BIT(6)
 
 void s_init(void)
 {
+	/* Enable writing to PFC and PMC registers */
+       *(volatile u32 *)PFC_PWPR = *(volatile u32 *)PFC_PWPR | PFC_PWPR_REGWE_A;
+
 #if CONFIG_TARGET_RZG3E_DEV
-       *(volatile u8 *)PMC_34   &= ~(0x06<<4); /* PK1,PK2 port */
-       *(volatile u8 *)P_34      = (*(volatile u32 *)P_34  & ~(0x03<<4)) | (0x01 <<5); /* PK1=1,PK2=0          */
-       *(volatile u16 *)PM_34    = (*(volatile u32 *)PM_34 & ~(0x0f<<8)) | (0x0c <<8); /* PK1,PK2 output       */
+	/* QSD1 */
+	*(volatile u8 *)PMC(1) &= ~(BIT(5) | BIT(6)); /* P1_5, P1_6 */
+	*(volatile u8 *)P(1) = (*(volatile u8 *)P(1) & ~BIT(5)) | BIT(6); /* P1_5 = 0, P1_6 = 1 */
+	*(volatile u16 *)PM(1) = (*(volatile u16 *)PM(1) & ~GENMASK(13, 10)) | BIT(13) | BIT(11); /* P1_5, P1_6 output */
+
+	/* QSD2 */
+	*(volatile u8 *)PMC(K) &= ~(BIT(1) | BIT(2)); /* PK_1, PK_2 */
+	*(volatile u8 *)P(K) = (*(volatile u8 *)P(K) & ~BIT(1)) | BIT(2); /* PK_1 = 0, PK_2 = 1 */
+	*(volatile u16 *)PM(K) = (*(volatile u16 *)PM(K) & ~GENMASK(5, 2)) | BIT(5) | BIT(3); /* PK_1, PK_2 output */
 #endif
 
+#if CONFIG_TARGET_SMARC_RZG3E
+	/* QSD2 */
+	*(volatile u8 *)PMC(K) &= ~(BIT(1) | BIT(2)); /* PK_1, PK_2 */
+	*(volatile u8 *)P(K) = *(volatile u8 *)P(K) | BIT(1) | BIT(2); /* PK_1 = 1, PK_2 = 1 */
+	*(volatile u16 *)PM(K) = (*(volatile u16 *)PM(K) & ~GENMASK(5, 2)) | BIT(5) | BIT(3); /* PK_1, PK_2 output */
+#endif
 
+	/* Disable writing to PFC and PMC registers */
+       *(volatile u32 *)PFC_PWPR = *(volatile u32 *)PFC_PWPR & ~PFC_PWPR_REGWE_A;
 }
 
 int board_early_init_f(void)
