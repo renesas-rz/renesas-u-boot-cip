2f3a3d13dbfab9c889dfd9da4c5626ccede486a7
Shinji Hirai <shinji.hirai.zn@renesas.com>
2024-01-29 12:55:55 +0900

    board: renesas: rzv2n-dev: rzv2h-dev.c: Enable i2c8 for RZ/V2N Dev board
    Signed-off-by: Shinji Hirai <shinji.hirai.zn@renesas.com>

--- board/renesas/rzv2n-dev/rzv2n-dev.c
+++ board/renesas/rzv2n-dev/rzv2n-dev.c
@@ -30,10 +30,17 @@ DECLARE_GLOBAL_DATA_PTR;
 /* PFC */
 #define PFC_BASE			0x10410000
 
+#define PWPR				(PFC_BASE + 0x3C04)
+#define PWPR_REGWE_A			BIT(6)
+#define	PWPR_REGWE_B			BIT(5)
+
 #define	P_2A				(PFC_BASE + 0x002A)
 #define	PM_2A				(PFC_BASE + 0x0154)
 #define	PMC_2A				(PFC_BASE + 0x022A)
 #define PFC_OEN				(PFC_BASE + 0x3C40)
+
+#define	PMC_20				(PFC_BASE + 0x0220)
+#define	PFC_20				(PFC_BASE + 0x0480)
 
 /* CPG */
 #define CPG_BASE			0x10420000
@@ -42,15 +49,30 @@ DECLARE_GLOBAL_DATA_PTR;
 #define CPG_RESET_ETH			(CPG_BASE + 0x092C)
 #define CPG_RESETMON_ETH		(CPG_BASE + 0x0A14)
 
+#define	CPG_CLKON_9			(CPG_BASE + 0x0624)
+#define	CPG_RST_9			(CPG_BASE + 0x0924)
+#define	CPG_RST_10			(CPG_BASE + 0x0928)
+
 void s_init(void)
 {
 #if CONFIG_TARGET_RZV2N_DEV
+	*(volatile u32 *)PWPR |= (PWPR_REGWE_A | PWPR_REGWE_B);
+
 	/* PA5,PA4 port	*/
 	*(volatile u8 *)PMC_2A   &= ~(0x03 << 4);
 	/* PA5=1,PA4=0 */
 	*(volatile u8 *)P_2A      = (*(volatile u32 *)P_2A  & ~(0x03 << 4)) | (0x01 << 5);
 	/* PA5,PA4 output */
 	*(volatile u16 *)PM_2A    = (*(volatile u32 *)PM_2A & ~(0x0f << 8)) | (0x0c << 8);
+
+	/* I2C8 */
+	*(volatile u32 *)PFC_20  = (*(volatile u32 *)PFC_20 & 0x00FFFFFF) | (0x01 << 28) | (0x01 << 24);
+	*(volatile u8 *)PMC_20   |= (0x03) << 6;	/* P07,P06 multiplexed function	*/
+
+	*(volatile u32 *)PWPR &= ~(PWPR_REGWE_A | PWPR_REGWE_B);
+
+	*(volatile u32 *)CPG_CLKON_9 = 0x00080008;
+	*(volatile u32 *)CPG_RST_10  = 0x00010001;
 
 	/* Use PLL clock for clk_tx_i only for RGMII mode */
 	/* Wite OEN reg. OEN0 bit "0" for output direction */
