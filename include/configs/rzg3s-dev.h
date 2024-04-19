3a6bc92d6632f4f73e699204102ccdcd80e3f6f7
Nghia Nguyen <nghia.nguyen.xm@renesas.com>
2024-04-19 08:35:13 +0700

    configs: dev-rzg3s: Fix default env settings
    This patch revises some mistakes in the macro used to support booting
    from SD/eMMC for the default environment on RZ/G3S dev.
    
    Signed-off-by: Nghia Nguyen <nghia.nguyen.xm@renesas.com>

--- include/configs/rzg3s-dev.h
+++ include/configs/rzg3s-dev.h
@@ -66,10 +66,8 @@
 	"prodsdbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk1p2 \0" \
 	"prodemmcbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk0p2 \0" \
 	"bootimage=booti 0x48080000 - 0x48000000 \0" \
-	"emmcload=ext4load mmc 0:2 0x48080000 boot/Image \0" \
-	"ext4load mmc 0:2 0x48000000 boot/r9a08g045s33-dev.dtb;run prodemmcbootargs \0" \
-	"sd1load=ext4load mmc 1:2 0x48080000 boot/Image \0" \
-	"ext4load mmc 1:2 0x48000000 boot/r9a08g045s33-dev.dtb;run prodsdbootargs \0" \
+	"emmcload=ext4load mmc 0:2 0x48080000 boot/Image;ext4load mmc 0:2 0x48000000 boot/r9a08g045s33-dev.dtb;run prodemmcbootargs \0" \
+	"sd1load=ext4load mmc 1:2 0x48080000 boot/Image;ext4load mmc 1:2 0x48000000 boot/r9a08g045s33-dev.dtb;run prodsdbootargs \0" \
 	"bootcmd_check=if mmc dev 1; then run sd1load; else run emmcload; fi \0"
 
 #define CONFIG_BOOTCOMMAND	"env default -a;run bootcmd_check;run bootimage"
