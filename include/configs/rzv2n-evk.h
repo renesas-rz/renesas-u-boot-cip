bd549310d0034fb53a87d0728a28edd250b41b86
Hien Dang <hien.dang.xf@renesas.com>
2024-05-07 16:29:42 +0700

    include: configs: rzv2n-evk: Update default U-boot environment variables
    Update default U-boot environment variables for RZ/V2N EVK Board to
    make it boot from eSD by default.
    
    Signed-off-by: Hien Dang <hien.dang.xf@renesas.com>

--- include/configs/rzv2n-evk.h
+++ include/configs/rzv2n-evk.h
@@ -61,12 +61,15 @@
 /* ENV setting */
 #define CONFIG_EXTRA_ENV_SETTINGS	\
 	"usb_pgood_delay=2000\0" \
-	"bootm_size=0x10000000\0"
+	"bootm_size=0x10000000\0" \
+	"prodsdbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk1p2 \0" \
+	"prodemmcbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk0p2 \0" \
+	"bootimage=booti 0x48080000 - 0x48000000 \0" \
+	"emmcload=ext4load mmc 0:2 0x48080000 boot/Image;ext4load mmc 0:2 0x48000000 boot/r9a09g056n44-evk.dtb;run prodemmcbootargs \0" \
+	"sd1load=ext4load mmc 1:2 0x48080000 boot/Image;ext4load mmc 1:2 0x48000000 boot/r9a09g056n44-evk.dtb;run prodsdbootargs \0" \
+	"bootcmd_check=if mmc dev 1; then run sd1load; else run emmcload; fi \0"
 
-#define CONFIG_BOOTCOMMAND	\
-	"tftp 0x48080000 Image; " \
-	"tftp 0x48000000 Image-"CONFIG_DEFAULT_FDT_FILE"; " \
-	"booti 0x48080000 - 0x48000000"
+#define CONFIG_BOOTCOMMAND	"env default -a;run bootcmd_check;run bootimage"
 
 /* For board */
 /* Ethernet RAVB */
