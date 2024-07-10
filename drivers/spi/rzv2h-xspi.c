0b7a5496f5a4918f1fe2f3cb48c7ba7bde36cc96
Linh Hua <linh.hua.jc@renesas.com>
2024-07-10 20:17:57 +0700

    drivers: spi: rzv2h-xspi: Add support xSPI for RZ/G3E
    This commit supports xSPI driver for RZ/G3E SoC.
    
    Signed-off-by: Linh Hua <linh.hua.jc@renesas.com>

--- drivers/spi/rzv2h-xspi.c
+++ drivers/spi/rzv2h-xspi.c
@@ -497,6 +497,7 @@ static const struct dm_spi_ops xspi_ops = {
 
 static const struct udevice_id xspi_ids[] = {
 	{ .compatible = "renesas,xspi-r9a09g057" },
+	{ .compatible = "renesas,xspi-r9a09g047" },
 	{ }
 };
 
