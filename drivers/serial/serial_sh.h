ec1b5439131edb17c3e2b282c2290d5c1a5caf2d
Hoa Huynh <hoa.huynh.ud@renesas.com>
2024-07-24 16:39:42 +0700

    drivers: serial_sh: Support access 32bits registers
    Signed-off-by: Hoa Huynh <hoa.huynh.ud@renesas.com>

--- drivers/serial/serial_sh.h
+++ drivers/serial/serial_sh.h
@@ -3,6 +3,7 @@
  */
 
 #include <dm/platform_data/serial_sh.h>
+#include <linux/bitops.h>
 
 struct uart_port {
 	unsigned long	iobase;		/* in/out[bwl] */
@@ -89,9 +90,10 @@ struct uart_port {
 # define SCSPTR7 0xe800a820 /* 16 bit SCIF */
 # define SCSCR_INIT(port)	0x38 /* TIE=0,RIE=0,TE=1,RE=1,REIE=1 */
 # define SCIF_ORER 0x0001  /* overrun error bit */
-#elif defined(CONFIG_RCAR_GEN2) || defined(CONFIG_RCAR_64) || \
-      defined(CONFIG_R7S72100) || defined(CONFIG_RZG2L)
-# if defined(CFG_SCIF_A)
+#elif defined(CONFIG_RCAR_GEN2) || defined(CONFIG_RCAR_GEN3) || \
+      defined(CONFIG_R7S72100) || defined(CONFIG_R9A07G044L) || defined(CONFIG_R9A07G044C) || \
+      defined(CONFIG_R9A07G054L) || defined(CONFIG_R9A07G043U) || defined(CONFIG_R9A09G077) || defined(CONFIG_RZF_DEV)
+# if defined(CONFIG_SCIF_A)
 #  define SCIF_ORER	0x0200
 # else
 #  define SCIF_ORER	0x0001
@@ -234,20 +236,23 @@ struct uart_port {
 #define SCI_IN(size, offset)\
 	if ((size) == 8) {\
 		return readb(port->membase + (offset));\
-	} else {\
+	} else if ((size) == 16) {\
 		return readw(port->membase + (offset));\
+	} else if ((size) == 32) {\
+		return readl(port->membase + (offset));\
 	}
 #define SCI_OUT(size, offset, value)\
 	if ((size) == 8) {\
 		writeb(value, port->membase + (offset));\
 	} else if ((size) == 16) {\
 		writew(value, port->membase + (offset));\
+	} else if ((size) == 32) {\
+		writel(value, port->membase + (offset));\
 	}
 
 #define CPU_SCIx_FNS(name, sci_offset, sci_size, scif_offset, scif_size)\
 	static inline unsigned int sci_##name##_in(struct uart_port *port) {\
-		if (port->type == PORT_SCIF || port->type == PORT_SCIFB ||\
-		    port->type == PORT_HSCIF) {\
+		if (port->type == PORT_SCIF || port->type == PORT_SCIFB || port->type == PORT_RSCI) {\
 			SCI_IN(scif_size, scif_offset)\
 		} else { /* PORT_SCI or PORT_SCIFA */\
 			SCI_IN(sci_size, sci_offset);\
@@ -255,8 +260,7 @@ struct uart_port {
 	}\
 static inline void sci_##name##_out(struct uart_port *port,\
 				unsigned int value) {\
-	if (port->type == PORT_SCIF || port->type == PORT_SCIFB ||\
-	    port->type == PORT_HSCIF) {\
+	if (port->type == PORT_SCIF || port->type == PORT_SCIFB || port->type == PORT_RSCI) {\
 		SCI_OUT(scif_size, scif_offset, value)\
 	} else {	/* PORT_SCI or PORT_SCIFA */\
 		SCI_OUT(sci_size, sci_offset, value);\
@@ -312,9 +316,9 @@ static inline void sci_##name##_out(struct uart_port *port,\
 					sh4_scif_offset, sh4_scif_size)
 		#define SCIF_FNS(name, sh4_scif_offset, sh4_scif_size) \
 			CPU_SCIF_FNS(name, sh4_scif_offset, sh4_scif_size)
-#elif defined(CONFIG_RZG2L)
-#define SCIF_FNS(reg_name, reg_offset, reg_size) \
-	CPU_SCIF_FNS(reg_name, reg_offset, reg_size)
+#elif defined(CONFIG_R9A07G044L) || defined(CONFIG_R9A07G044C) || defined(CONFIG_R9A07G054L) || defined(CONFIG_R9A07G043U) || defined(CONFIG_R9A09G077) || defined(CONFIG_RZF_DEV)
+	#define SCIF_FNS(reg_name, reg_offset, reg_size) \
+		CPU_SCIF_FNS(reg_name, reg_offset, reg_size)
 #else
 #define SCIx_FNS(name, sh3_sci_offset, sh3_sci_size,\
 				sh4_sci_offset, sh4_sci_size, \
@@ -390,7 +394,7 @@ SCIF_FNS(SCLSR,  0,  0, 0x14, 16)
 #else
 SCIF_FNS(SCLSR,  0,  0, 0x24, 16)
 #endif
-#elif defined(CONFIG_RZG2L)
+#elif defined(CONFIG_R9A07G044L) || defined(CONFIG_R9A07G044C) || defined(CONFIG_R9A07G054L) || defined(CONFIG_R9A07G043U) || defined(CONFIG_R9A09G077) || defined(CONFIG_RZF_DEV)
 SCIF_FNS(SCSMR,  0x00, 16)
 SCIF_FNS(SCBRR,  0x02,  8)
 SCIF_FNS(SCSCR,  0x04, 16)
@@ -403,6 +407,51 @@ SCIF_FNS(SCSPTR, 0x10, 16)
 SCIF_FNS(SCLSR,  0x12, 16)
 SCIF_FNS(SCSEMR, 0x14,  8)
 SCIF_FNS(SCxTCR, 0x16, 16)
+
+#if defined(CONFIG_R9A09G077)
+/* RSCI registers */
+SCIF_FNS(RSCxRDR, 0x00, 32)
+SCIF_FNS(RSCxTDR, 0x04, 32)
+SCIF_FNS(CCR0,	  0x08, 32)
+SCIF_FNS(CCR1,	  0x0C, 32)
+SCIF_FNS(CCR2,	  0x10, 32)
+SCIF_FNS(CCR3,	  0x14, 32)
+SCIF_FNS(CCR4,	  0x18, 32)
+SCIF_FNS(RSCFCR,  0x24, 32)
+SCIF_FNS(RSCxSR,  0x48, 32)
+SCIF_FNS(FRSR,	  0x50, 32)
+SCIF_FNS(CFCLR,	  0x68, 32)
+SCIF_FNS(FFCLR,	  0x70, 32)
+
+#define FRSR_RFDC_MASK			0x00003F00 		/* Receive FIFO Data Count */
+#define FRSR_RFDC_SHIFT			8
+#define FRSR_DR   			BIT(0)
+#define FFCLR_DRC			BIT(0)
+
+#define CCR0_INIT			(BIT(4)|BIT(0))  	/* TE=1, RE=1 */
+
+#define CCR2_INIT_BRR			0xFF002104		/* Bit rate setting: 115200bps */
+
+#define CCR3_INIT			0x00001283
+#define CCR3_FM				BIT(20)			/* FIFO Mode select */
+
+#define FCR_INIT			0x1F1F0000
+#define FCR_RFRST 			BIT(23)			/* Receive FIFO Data Register Reset */
+#define FCR_TFRST 			BIT(15)			/* Transmit FIFO Data Register Reset */
+
+#define CSR_RDRF 			BIT(31)
+#define CSR_TEND 			BIT(30)
+#define CSR_FER 			BIT(28)
+#define CSR_PER 			BIT(27)
+#define CSR_ORER 			BIT(24)
+#define CSR_ERRORS 			(CSR_PER | CSR_FER)
+
+#define CFCLR_RDRFC			BIT(31)
+#define CFCLR_TDREC			BIT(29)
+#define CFCLR_ORERC			BIT(24)
+
+#endif
+
 SCIF_FNS(DL,     0x00,  0)
 #else
 /*      reg      SCI/SH3   SCI/SH4  SCIF/SH3   SCIF/SH4  SCI/H8*/
@@ -503,7 +552,7 @@ static inline int sci_rxd_in(struct uart_port *port)
 #elif defined(CONFIG_CPU_SH7723)
 static inline int scbrr_calc(struct uart_port *port, int bps, int clk)
 {
-	if (port->type == PORT_SCIF)
+	if (port->type == PORT_SCIF || port->type == PORT_RSCI)
 		return (clk+16*bps)/(32*bps)-1;
 	else
 		return ((clk*2)+16*bps)/(16*bps)-1;
