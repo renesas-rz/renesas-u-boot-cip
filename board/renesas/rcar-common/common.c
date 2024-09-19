3e646aaa9b4cbbaa2bb82a5f9a0706dfbdaab570
Tai Huynh <tai.huynh.cp@renesas.com>
2024-09-19 13:21:50 +0700

    rcar-common: common: apply workaround for RZ/N2H
    Signed-off-by: Tai Huynh <tai.huynh.cp@renesas.com>

--- board/renesas/rcar-common/common.c
+++ board/renesas/rcar-common/common.c
@@ -24,14 +24,8 @@ DECLARE_GLOBAL_DATA_PTR;
 /* If the firmware passed a device tree use it for e.g. U-Boot DRAM setup. */
 extern u64 rcar_atf_boot_args[];
 
-<<<<<<< HEAD
-#define FDT_RPC_PATH	"/soc/spi@ee200000"
-
-static void apply_atf_overlay(void *fdt_blob)
-=======
 #if !(defined(CONFIG_R9A07G044L) || defined(CONFIG_R9A07G044C) || defined(CONFIG_R9A07G054L) || defined(CONFIG_R9A07G043U) || (defined CONFIG_R9A09G077))
 int fdtdec_board_setup(const void *fdt_blob)
->>>>>>> rcar-common: common: apply workaround for RZ/T2H
 {
 	void *atf_fdt_blob = (void *)(rcar_atf_boot_args[1]);
 
