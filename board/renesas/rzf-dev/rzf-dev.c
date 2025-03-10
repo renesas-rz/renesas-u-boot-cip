98a5cee0834b5028119534a976192a1f32e6531b
Linh Hua <linh.hua.jc@renesas.com>
2025-03-10 09:31:22 +0700

    board: renesas: rzf-dev: Add ft_board_setup support for RZ/Five
    Add empty ft_board_setup() for future FDT customizations.
    
    Signed-off-by: Linh Hua <linh.hua.jc@renesas.com>

--- board/renesas/rzf-dev/rzf-dev.c
+++ board/renesas/rzf-dev/rzf-dev.c
@@ -40,6 +40,13 @@ static void v5l2_init(void)
 	uclass_get_device(UCLASS_CACHE, 0, &dev);
 }
 
+#if defined(CONFIG_OF_BOARD_SETUP)
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+	return 0;
+}
+#endif
+
 static int sd_power_setup(void)
 {
 	int ret = 0;
