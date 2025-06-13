tq-systems/u-boot-tqmaxx

drivers: sdhi: Add support sdhi for RZ-Five Dev
This patch add sdhi support for RZ-Five Dev

Signed-off-by: LongLuu <long.luu.ur@renesas.com>

drivers/mmc/sh_sdhi.c
@@ -22,7 +22,12 @@
 #include <linux/compat.h>
 #include <linux/io.h>
 #include <linux/sizes.h>
+#ifdef CONFIG_ARCH_RMOBILE
 #include <asm/arch/rmobile.h>
+#endif
+#ifdef CONFIG_RZF_DEV
+#include <asm/arch-rzmpu/sh_sdhi.h>
+#endif
 #include <asm/arch/sh_sdhi.h>
 #include <asm/global_data.h>
 #include <clk.h>

renesas-rz/renesas-u-boot-cip

drivers/mmc/sh_sdhi.c
@@ -22,7 +22,12 @@
 #include <linux/compat.h>
 #include <linux/io.h>
 #include <linux/sizes.h>
+#ifdef CONFIG_ARCH_RENESAS
 #include <asm/arch/rmobile.h>
+#endif
+#ifdef CONFIG_RZF_DEV
+#include <asm/arch-rzmpu/sh_sdhi.h>
+#endif
 #include <asm/arch/sh_sdhi.h>
 #include <asm/global_data.h>
 #include <clk.h>