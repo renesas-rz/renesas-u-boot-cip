82a51cf7b23edc6c4475a688d0929d506726ef6d
John Vincent <john.vincent.xa@bp.renesas.com>
2025-07-11 18:26:33 +0100

    RZT2H and RZN2H ARM system ready configs
    ARM system ready configs added
    
    Signed-off-by: John Vincent <john.vincent.xa@bp.renesas.com>

--- include/configs/rzn2h-dev.h
+++ include/configs/rzn2h-dev.h
@@ -61,9 +61,9 @@
 	"bootimage=booti 0xc4200000 - 0xC5F00000\0" \
 	"emmcload=ext4load mmc 0:2 0xc4200000 boot/Image;ext4load mmc 0:2 0xC5F00000 boot/r9a09g087m44-dev.dtb;run prodemmcbootargs \0" \
 	"sd1load=ext4load mmc 1:2 0xc4200000 boot/Image;ext4load mmc 1:2 0xC5F00000 boot/r9a09g087m44-dev.dtb;run prodsdbootargs \0" \
-	"bootcmd_check=if mmc dev 1; then run sd1load; else run emmcload; fi \0"
-
-#define CONFIG_BOOTCOMMAND	"env default -a;run bootcmd_check;run bootimage"
+	"bootcmd_check=if mmc dev 1; then run sd1load; else run emmcload; fi \0" \
+	"dfu_alt_info=sf 0:0=fip.bin raw 0x60000 0x1F0000 \0" \
+	"dfu_bufsiz=0x1F0000\0" \
 
 /* For board */
 /* Ethernet RAVB */
