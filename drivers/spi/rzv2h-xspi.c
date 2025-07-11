0f488228dc08325188454066e9ad88e49d4163c2
John Vincent <john.vincent.xa@bp.renesas.com>
2025-07-11 15:04:31 +0100

    RZN2H ethernet and xspi0
    PFC register address settings in device tree
    default env variable name correction
    
    Signed-off-by: John Vincent <john.vincent.xa@bp.renesas.com>

--- drivers/spi/rzv2h-xspi.c
+++ drivers/spi/rzv2h-xspi.c
@@ -505,6 +505,7 @@ static const struct udevice_id xspi_ids[] = {
 	{ .compatible = "renesas,xspi-r9a08g046" },
 	{ .compatible = "renesas,xspi-r9a09g047" },
 	{ .compatible = "renesas,t2h-xspi-if" },
+	{ .compatible = "renesas,n2h-xspi-if" },
 	{ }
 };
 
