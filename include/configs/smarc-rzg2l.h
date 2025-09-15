9fad4dd103c635203a1b4c87ef9c6beec8eb0096
John Vincent <john.vincent.xa@bp.renesas.com>
2025-09-15 11:45:27 +0100

    rzg2l: add distro_bootcmd to seach sd and usb
    scan efi binaries from the removable disk and boot them
    if they are present
    
    Signed-off-by: John Vincent <john.vincent.xa@bp.renesas.com>

--- include/configs/smarc-rzg2l.h
+++ include/configs/smarc-rzg2l.h
@@ -40,6 +40,66 @@
 /* SDHI clock freq */
 #define CONFIG_SH_SDHI_FREQ		133000000
 
+
+/* ENV setting */
+#if defined(CONFIG_RZG2L_DISTRO_BOOT)
+#define CFG_EXTRA_ENV_SETTINGS	\
+	"usb_pgood_delay=2000\0" \
+	"fdt_addr_r=0x48000000\0" \
+	"fdtfile="CONFIG_DEFAULT_FDT_FILE"\0" \
+	"kernel_addr_r=0x48080000\0" \
+	"boot_efi_binary=efi/boot/bootaa64.efi\0" \
+	"scan_for_usb_dev=" \
+		"usb start; " \
+		"if test ! -e usb ${devnum}:1 /; then usb reset; fi;\0" \
+	"scan_boot_efi=" \
+		"part list ${devtype} ${devnum} devplist; "  \
+		"env exists devplist || setenv devplist 1; " \
+		"for distro_bootpart in ${devplist}; do " \
+			"if test -e ${devtype} ${devnum}:${distro_bootpart} ${boot_efi_binary}; then " \
+				"load ${devtype} ${devnum}:${distro_bootpart} " \
+				"${kernel_addr_r} ${boot_efi_binary};"          \
+				"echo BootEFI from <${devtype}> [${devnum}:${distro_bootpart}]; "\
+				"bootefi ${kernel_addr_r};"                     \
+			"fi;" \
+		"done;\0" \
+	"mmc0=" \
+			"setenv devnum 0;" \
+			"setenv devtype mmc;" \
+			"run scan_boot_efi;\0" \
+	"mmc1=" \
+			"setenv devnum 1;" \
+			"setenv devtype mmc;" \
+			"run scan_boot_efi;\0" \
+	"usb0=" \
+			"setenv devnum 0;" \
+			"setenv devtype usb;" \
+			"run scan_for_usb_dev;"\
+			"run scan_boot_efi;\0"\
+	"usb1=" \
+			"setenv devnum 1;" \
+			"setenv devtype usb;" \
+			"run scan_for_usb_dev;"\
+			"run scan_boot_efi;\0" \
+	"boot_targets=" \
+			"usb0 usb1 mmc0 mmc1\0" \
+	"dfu_alt_info=" \
+			"sf 0:0=fip.bin raw 0x60000 0x1F0000\0" \
+	"dfu_bufsiz=" \
+			"0x1F0000\0" \
+	"ipaddr=" \
+			"192.168.10.7\0" \
+	"serverip=" \
+			"192.168.10.3\0" \
+	"distro_bootcmd=" \
+			"env exists boot_targets || setenv boot_targets mmc0 mmc1 usb0 usb1; " \
+			"for target in ${boot_targets}; do "\
+				"run ${target};" \
+			"done;" \
+	"bootcmd=run distro_bootcmd\0"
+
+#else
+
 /* The HF/QSPI layout permits up to 1 MiB large bootloader blob */
 #define CONFIG_BOARD_SIZE_LIMIT		1048576
 
@@ -57,6 +117,7 @@
 
 #define CONFIG_BOOTCOMMAND	"env default -a;run bootcmd_check;run bootimage"
 
+#endif
 /* For board */
 /* Ethernet RAVB */
 #define CONFIG_BITBANGMII_MULTI
