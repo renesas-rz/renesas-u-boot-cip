1dd584b29270e0742a9c2473201e78d60e815a08
Alan Mcloughlin <alan.mcloughlin@renesas.com>
2025-10-15 21:27:06 +0100

    rzv2l: sdhi and pfc support linux dt
    No change is needed in the  driver. It now supports
    linux dt entries. Just add the compatibility strings
    
    Signed-off-by: Alan Mcloughlin <alan.mcloughlin@renesas.com>

--- drivers/mmc/sh_sdhi.c
+++ drivers/mmc/sh_sdhi.c
@@ -928,6 +928,7 @@ static const struct udevice_id sh_sdhi_sd_match[] = {
 	{ .compatible = "renesas,sdhi-r9a07g044l", .data = SH_SDHI_QUIRK_64BIT_BUF },
 	{ .compatible = "renesas,sdhi-r9a07g044", .data = SH_SDHI_QUIRK_64BIT_BUF },
 	{ .compatible = "renesas,sdhi-r9a07g054l", .data = SH_SDHI_QUIRK_64BIT_BUF },
+	{ .compatible = "renesas,sdhi-r9a07g054", .data = SH_SDHI_QUIRK_64BIT_BUF },
 	{ .compatible = "renesas,sdhi-r9a07g044c", .data = SH_SDHI_QUIRK_64BIT_BUF },
 	{ .compatible = "renesas,sdhi-r9a07g043u", .data = SH_SDHI_QUIRK_64BIT_BUF },
 	{ .compatible = "renesas,sdhi-r9a07g043f", .data = SH_SDHI_QUIRK_64BIT_BUF },
