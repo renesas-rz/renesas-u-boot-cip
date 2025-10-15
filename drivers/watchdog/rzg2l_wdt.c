96b6e00a5046927ca94cee6c6716b0427224bf4e
Alan Mcloughlin <alan.mcloughlin@renesas.com>
2025-10-15 21:11:17 +0100

    rzv2l: watchdog driver gets to support linut dt entries
    The linux dt is being supported in u-boot
    The watchdog drivers support them, no change needed in the
    driver, just add compatible string
    
    Signed-off-by: Alan Mcloughlin <alan.mcloughlin@renesas.com>

--- drivers/watchdog/rzg2l_wdt.c
+++ drivers/watchdog/rzg2l_wdt.c
@@ -61,6 +61,7 @@ static const struct udevice_id rzg2l_wdt_match[] = {
 	{ .compatible = "renesas,r9a07g044l-wdt", },
 	{ .compatible = "renesas,r9a07g044-wdt", },
 	{ .compatible = "renesas,r9a07g054l-wdt", },
+	{ .compatible = "renesas,r9a07g054-wdt", },
 	{ .compatible = "renesas,r9a07g043u-wdt", },
 	{ .compatible = "renesas,r9a07g043f-wdt", },
 	{ /* sentinel */ }
