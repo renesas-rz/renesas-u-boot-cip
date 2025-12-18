c7f6b4d0c4a602871877172eb416718c278ac55e
John Vincent <john.vincent.xa@bp.renesas.com>
2025-12-18 09:48:57 +0000

    rzv2h:distros:config Add RZ/V2H distro boot support
    Introduce CONFIG_RZV2H_DISTRO_BOOT option in Kconfig.rcar3 to enable
    distro boot command for RZ/V2H platforms. When selected, rzv2h-dev.h
    provides extended environment settings including USB power-good delay,
    default FDT file, kernel load address, EFI boot binary path, and logic
    to scan MMC/USB devices for EFI binaries. This adds a standard
    distro_bootcmd sequence with boot_targets for mmc0, mmc1, usb0, and usb1.
    
    The distro config for v2h enabled in its new default configs
    
    Signed-off-by: John Vincent <john.vincent.xa@bp.renesas.com>

--- include/configs/rzv2h-dev.h
+++ include/configs/rzv2h-dev.h
@@ -47,6 +47,55 @@
 #define CONFIG_BOARD_SIZE_LIMIT		1048576
 
 /* ENV setting */
+#if defined(CONFIG_RZV2H_DISTRO_BOOT)
+#define CFG_EXTRA_ENV_SETTINGS	\
+	"usb_pgood_delay=2000\0" \
+	"fdtfile="CONFIG_DEFAULT_FDT_FILE"\0" \
+	"kernel_addr_r=0x48080000\0" \
+	"boot_efi_binary=efi/boot/bootaa64.efi\0" \
+	"scan_for_usb_dev=" \
+		"usb start; " \
+		"if test ! -e usb ${devnum}:1 /; then usb reset; fi;\0" \
+	"scan_boot_efi=" \
+		"part list ${devtype} ${devnum} devplist; "  \
+		"env exists devplist || setenv devplist 1; " \
+		"for distro_bootpart in ${devplist}; do " \
+			"if test -e ${devtype} ${devnum}:${distro_bootpart} ${boot_efi_binary}; then " \
+				"load ${devtype} ${devnum}:${distro_bootpart} " \
+				"${kernel_addr_r} ${boot_efi_binary};"          \
+				"echo BootEFI from <${devtype}> [${devnum}:${distro_bootpart}]; "\
+				"bootefi ${kernel_addr_r};"                     \
+			"fi;" \
+		"done;\0" \
+	"mmc0=" \
+			"setenv devnum 0;" \
+			"setenv devtype mmc;" \
+			"run scan_boot_efi;\0" \
+	"mmc1=" \
+			"setenv devnum 1;" \
+			"setenv devtype mmc;" \
+			"run scan_boot_efi;\0" \
+	"usb0=" \
+			"setenv devnum 0;" \
+			"setenv devtype usb;" \
+			"run scan_for_usb_dev;"\
+			"run scan_boot_efi;\0"\
+	"usb1=" \
+			"setenv devnum 1;" \
+			"setenv devtype usb;" \
+			"run scan_for_usb_dev;"\
+			"run scan_boot_efi;\0" \
+	"boot_targets=" \
+			"usb0 usb1 mmc0 mmc1\0" \
+	"distro_bootcmd=" \
+			"env exists boot_targets || setenv boot_targets mmc0 mmc1 usb0 usb1; " \
+			"for target in ${boot_targets}; do "\
+				"run ${target};" \
+			"done;" \
+	"bootcmd=run distro_bootcmd\0"
+
+#else
+
 #if defined(CONFIG_TARGET_RZV2H_DEV)
 #define CFG_EXTRA_ENV_SETTINGS	\
 	"usb_pgood_delay=2000\0"	\
@@ -81,6 +130,8 @@
 #endif
 
 #define CONFIG_BOOTCOMMAND	"env default -a;run bootcmd_check;run bootimage"
+
+#endif
 
 /* For board */
 /* Ethernet RAVB */
