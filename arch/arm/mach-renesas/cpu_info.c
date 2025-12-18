tq-systems/u-boot-tqmaxx

arm: mach-rmobile: add information about RZ/G2M v3.0
This commit is created to add information about RZ/G2M v3.0
(a.k.a r8a774a3).
Also add display info about specific case of RZ/G2M v1.1/v1.2.

Signed-off-by: hienhuynh <hien.huynh.px@renesas.com>

arch/arm/mach-rmobile/cpu_info.c
@@ -106,10 +106,16 @@ int arch_misc_init(void)
 	const u8 *cpu_name = get_cpu_name(idx);
 	char cpu[10] = { 0 };
 
-	for (i = 0; i < sizeof(cpu); i++)
-		cpu[i] = tolower(cpu_name[i]);
+	if ((rmobile_cpuinfo[idx].cpu_type == RMOBILE_CPU_TYPE_R8A7796) &&
+	    (rmobile_get_cpu_rev_integer() == 3) &&
+	    (rmobile_get_cpu_rev_fraction() == 0)) {
+		env_set("platform", "r8a774a3");
+	} else {
+		for (i = 0; i < sizeof(cpu); i++)
+			cpu[i] = tolower(cpu_name[i]);
 
-	env_set("platform", cpu);
+		env_set("platform", cpu);
+	}
 
 	return 0;
 }
@@ -118,6 +124,22 @@ int arch_misc_init(void)
 int print_cpuinfo(void)
 {
 	int i = rmobile_cpuinfo_idx();
+
+	/* Specific cases for RZ/G2M */
+	if (rmobile_cpuinfo[i].cpu_type == RMOBILE_CPU_TYPE_R8A7796) {
+		if ((rmobile_get_cpu_rev_integer() == 1) &&
+		    (rmobile_get_cpu_rev_fraction() == 1)) {
+			printf("CPU:   Renesas Electronics %s rev 1.1/rev 1.2\n",
+				get_cpu_name(i));
+			return 0;
+		}
+
+		if ((rmobile_get_cpu_rev_integer() == 3) &&
+		    (rmobile_get_cpu_rev_fraction() == 0)) {
+			printf("CPU:   Renesas Electronics R8A774A3\n");
+			return 0;
+		}
+	}
 
 	printf("CPU:   Renesas Electronics %s rev %d.%d\n",
 		get_cpu_name(i), rmobile_get_cpu_rev_integer(),

renesas-rz/renesas-u-boot-cip

arch/arm/mach-renesas/cpu_info.c
@@ -102,11 +102,16 @@ int arch_misc_init(void)
 	const u8 *cpu_name = get_cpu_name(idx);
 	char cpu[10] = { 0 };
 
-	for (i = 0; i < sizeof(cpu); i++)
-		cpu[i] = tolower(cpu_name[i]);
+	if ((renesas_cpuinfo[idx].cpu_type == RENESAS_CPU_TYPE_R8A7796) &&
+	    (renesas_get_cpu_rev_integer() == 3) &&
+	    (renesas_get_cpu_rev_fraction() == 0)) {
+		env_set("platform", "r8a774a3");
+	} else {
+		for (i = 0; i < sizeof(cpu); i++)
+			cpu[i] = tolower(cpu_name[i]);
 
-	env_set("platform", cpu);
-
+		env_set("platform", cpu);
+	}
 	return 0;
 }
 #endif
@@ -115,11 +120,20 @@ int print_cpuinfo(void)
 {
 	int i = renesas_cpuinfo_idx();
 
-	if (renesas_cpuinfo[i].cpu_type == RENESAS_CPU_TYPE_R8A7796 &&
-	    renesas_get_cpu_rev_integer() == 1 &&
-	    renesas_get_cpu_rev_fraction() == 1) {
-		printf("CPU:   Renesas Electronics %s rev 1.1/1.2\n", get_cpu_name(i));
-		return 0;
+	/* Specific cases for RZ/G2M */
+	if (renesas_cpuinfo[i].cpu_type == RENESAS_CPU_TYPE_R8A7796) {
+		if ((renesas_get_cpu_rev_integer() == 1) &&
+		    (renesas_get_cpu_rev_fraction() == 1)) {
+			printf("CPU:   Renesas Electronics %s rev 1.1/rev 1.2\n",
+				get_cpu_name(i));
+			return 0;
+		}
+
+		if ((renesas_get_cpu_rev_integer() == 3) &&
+		    (renesas_get_cpu_rev_fraction() == 0)) {
+			printf("CPU:   Renesas Electronics R8A774A3\n");
+			return 0;
+		}
 	}
 
 	printf("CPU:   Renesas Electronics %s rev %d.%d\n",