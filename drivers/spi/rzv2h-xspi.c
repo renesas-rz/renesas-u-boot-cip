11f8de863bad6e44a3c4ee28a6565256205ca7c9
John Vincent <john.vincent.xa@bp.renesas.com>
2026-01-12 16:07:46 +0000

    rzv2h: xspi: add support for r9a09g057-xspi compatible
    Add the "renesas,r9a09g057-xspi" compatible string to the XSPI driver
    so that it can bind to the upstream device tree entry used on the
    RZ/V2H (r9a09g057) SoC. This ensures proper driver matching with the
    updated DTS bindings as per upstream linux kernel
    
    Signed-off-by: John Vincent <john.vincent.xa@bp.renesas.com>

--- drivers/spi/rzv2h-xspi.c
+++ drivers/spi/rzv2h-xspi.c
@@ -500,6 +500,7 @@ static const struct udevice_id xspi_ids[] = {
 	{ .compatible = "renesas,xspi-r9a09g057" },
 	{ .compatible = "renesas,xspi-r9a09g056" },
 	{ .compatible = "renesas,xspi-r9a09g047" },
+	{ .compatible = "renesas,r9a09g057-xspi" },
 	{ }
 };
 
