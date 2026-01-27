42d684897c1031c760fba897a396e67baaddbb8f
Alan Mcloughlin <alan.mcloughlin@renesas.com>
2026-01-27 12:26:49 +0000

    rzg2ul: capsule support for bl2 and fip
    The capsule details added for rzg2ul
    
    Signed-off-by: Alan Mcloughlin <alan.mcloughlin@renesas.com>

--- board/renesas/rzg2ul-dev/rzg2ul-dev.c
+++ board/renesas/rzg2ul-dev/rzg2ul-dev.c
@@ -20,8 +20,13 @@
 #include <asm/arch/sh_sdhi.h>
 #include <i2c.h>
 #include <mmc.h>
-#include <wdt.h>
 #include <rzg2l_wdt.h>
+#include <wdt.h>
+#include <spi.h>
+#include <spi-mem.h>
+#include <linux/mtd/spi-nor.h>
+#include "../rzg-common/common.h"
+#include <efi_loader.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -79,6 +84,36 @@ DECLARE_GLOBAL_DATA_PTR;
 /* WDT */
 #define WDT_INDEX		0
 
+#if IS_ENABLED(CONFIG_EFI_HAVE_CAPSULE_SUPPORT)
+
+#define EFI_FIRMWARE_IMAGE_TYPE_RZG2UL_GUID\
+    EFI_GUID(0x8b62f3c0, 0x707f, 0x4a65, 0x9b, 0x84, \
+             0x1f, 0x0a, 0x4c, 0x12, 0xd7, 0xa6)
+
+struct efi_fw_image fw_images[] = {
+	{
+		.image_type_id = EFI_FIRMWARE_IMAGE_TYPE_RZG2UL_GUID,
+		.fw_name = u"bl2_bp-smarc-rzg2ul_pmic.bin",
+		.image_index = 1,
+	},
+	{
+		.image_type_id = EFI_FIRMWARE_IMAGE_TYPE_RZG2UL_GUID,
+		.fw_name = u"fip-smarc-rzg2ul_pmic.bin",
+		.image_index = 2,
+	},
+};
+
+struct efi_capsule_update_info update_info = {
+	.dfu_string =
+		/* BL2 in SPI NOR at offset 0x0, max size 0x20000  */
+		"sf 0:0=bl2_bp-smarc-rzg2ul_pmic.bin raw 0x0 0x20000;"
+		/* FIP in SPI NOR at offset 0x60000, max size 0x1F0000 (1984 KB) */
+		"fip-smarc-rzg2ul_pmic.bin raw 0x20000 0x1F0000",
+	.num_images = ARRAY_SIZE(fw_images),
+	.images = fw_images,
+};
+
+#endif /* EFI_HAVE_CAPSULE_SUPPORT */
 /* ECC */
 #define DDR_MEMC_BASE		(0x11410000)
 #define ECC_ENABLE_ADDR		(0x0174)
