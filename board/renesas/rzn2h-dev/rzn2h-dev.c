4d3bfd97be7331e216fca3e6892e450a2327ad0f
John Vincent <john.vincent.xa@bp.renesas.com>
2026-02-03 08:13:18 +0000

    board: rzt2h-dev: rzn2h-dev: Configure xSPI0 CS0 end address
    The RZ/T2H, RZ/N2H EVK uses an MX25LW51245GXDI00‑T Octal Flash
    device on xSPI0. This patch programs the correct CS0
    end‑address range (64 MiB) for xSPI0 by setting
    CS0ENDAD_xSPI(0) to 0x43FFFFFF.
    
    This make sure u-boot xspi0 register settings work independent of
    tf-a settings in mode xspi, emmc and sd boot.
    
    Signed-off-by: John Vincent <john.vincent.xa@bp.renesas.com>

--- board/renesas/rzn2h-dev/rzn2h-dev.c
+++ board/renesas/rzn2h-dev/rzn2h-dev.c
@@ -177,8 +177,12 @@ void s_init(void)
         *(volatile u32 *)PRCRN = PRCRN_PRKEY | PRCRN_WR_EN;
 	*(volatile u32 *)PRCRS = PRCRS_PRKEY | PRCRS_WR_EN;
 
-        /* Setting xSPI1 CS0 End Address */
-        *(volatile u32 *)CS0ENDAD_xSPI(1) = 0x57FFFFFF;
+	/* Setting xSPI0 CS0 End Address
+	Octal Flash MX25LW51245GXDI00-T 64 MiB range */
+	*(volatile u32 *)CS0ENDAD_xSPI(0) = 0x43FFFFFF;
+
+    /* Setting xSPI1 CS0 End Address */
+    *(volatile u32 *)CS0ENDAD_xSPI(1) = 0x57FFFFFF;
 
 	/* P2_5_SD0_PWEN_A Output HIGH */
 	*(volatile u8 *)PMC(2) |= BIT(5);
--- board/renesas/rzt2h-dev/rzt2h-dev.c
+++ board/renesas/rzt2h-dev/rzt2h-dev.c
@@ -179,6 +179,10 @@ void s_init(void)
 	*(volatile u32 *)PRCRN = PRCRN_PRKEY | PRCRN_WR_EN;
 	*(volatile u32 *)PRCRS = PRCRS_PRKEY | PRCRS_WR_EN;
 
+	/* Setting xSPI0 CS0 End Address
+	Octal Flash MX25LW51245GXDI00-T 64 MiB range */
+	*(volatile u32 *)CS0ENDAD_xSPI(0) = 0x43FFFFFF;
+
 	/* Setting xSPI1 CS0 End Address */
 	*(volatile u32 *)CS0ENDAD_xSPI(1) = 0x57FFFFFF;
 
