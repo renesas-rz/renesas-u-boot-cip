3b19df317be35f2240ff7d513f0f1407430d1071
John Vincent <john.vincent.xa@bp.renesas.com>
2026-02-04 14:25:49 +0000

    board: renesas: rzv2n-dev: remove unused PMIC I2C initialization
    The PMIC initialization sequence on I2C bus 8 is no longer
    required for the RZ/V2N platform.
    
    Due to hardware difference between evk variants the pmic
    initialization may cause linux kernel crash in other
    unsupported variants and hence the i2c pmic init is removed
    
    Signed-off-by: John Vincent <john.vincent.xa@bp.renesas.com>

--- board/renesas/rzv2n-dev/rzv2n-dev.c
+++ board/renesas/rzv2n-dev/rzv2n-dev.c
@@ -250,77 +250,9 @@ static void board_usb_init(void)
 	(*(volatile u32 *)(USB20_BASE + HcRhDescriptorA)) |= (0x1u << 12);
 }
 
-static void board_pmic_i2c_check_val(struct udevice *dev, u8 reg_addr, u8 reg_val)
-{
-	u8 read_val;
-
-	/* Read back the value from register */
-	if (dm_i2c_read(dev, reg_addr, &read_val, 1)) {
-		printf("Error: Failed to read back value from register 0x%x\n", reg_addr);
-		return;
-	}
-
-	/* Check if the value was written correctly */
-	if (read_val != reg_val)
-		printf("Error: Written value was not correctly at register 0x%x\n", reg_addr);
-}
-
 int board_late_init(void)
 {
-	struct udevice *dev;
-	const u8 pmic_i2c_bus = 8;
-	u8 reg;
-	int ret;
-
-	ret = i2c_get_chip_for_busnum(pmic_i2c_bus, 0x12, 1, &dev);
-
-	if (!ret)
-	{
-		dm_i2c_read(dev, 0x2E, &reg, 1);
-		reg &= (~0x01);
-
-		dm_i2c_write(dev, 0x2E, &reg, 1);
-
-		udelay(2);
-		reg |= (0x01);
-
-		dm_i2c_write(dev, 0x2E, &reg, 1);
-	}
-
-	return ret;
-}
-
-
-static void board_pmic_i2c_init(void)
-{
-	struct udevice *bus, *dev;
-	int ret;
-	u8 reg_addr, reg_val;
-
-	/* Get the I2C bus */
-	ret = uclass_get_device_by_seq(UCLASS_I2C, 8, &bus);
-	if (ret)
-		goto pmic_failed;
-
-	/* Initialize I2C device at address 0x6f */
-	ret = dm_i2c_probe(bus, 0x6f, 0, &dev);
-	if (ret)
-		goto pmic_failed;
-
-	/* Write 0x00 to register 0x24 of device 0x6f */
-	reg_addr = 0x24;
-	reg_val = 0x00;
-	ret = dm_i2c_write(dev, reg_addr, &reg_val, 1);
-	if (ret)
-		goto pmic_failed;
-
-	board_pmic_i2c_check_val(dev, reg_addr, reg_val);
-
-	return;
-
-pmic_failed:
-	printf("Can not initialize PMIC settings via I2C8\n");
-	return;
+	return 0;
 }
 
 int board_early_init_f(void)
@@ -334,11 +266,6 @@ int board_init(void)
 	gd->bd->bi_boot_params = CONFIG_TEXT_BASE + 0x50000;
 
 	board_usb_init();
-
-	/* Initialize PMIC I2C devices */
-#if (CONFIG_TARGET_RZV2N_EVK)
-	board_pmic_i2c_init();
-#endif
 
 	return 0;
 }
