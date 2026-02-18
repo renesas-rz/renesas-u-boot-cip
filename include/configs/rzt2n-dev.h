1fe4019d4cfb884ba389a680f71b6b7e9531b1dd
John Vincent <john.vincent.xa@bp.renesas.com>
2026-02-18 16:20:15 +0000

    rzt2n-dev: move FDT load address to avoid kernel overlap
    The previous environment configuration loaded the device tree at
    0xC5F00000, which lies inside the kernel image range as parsed by
    booti (OS = 0xC4200000..0xC7010000). This resulted in the runtime
    error:
    
      "ERROR: FDT image overlaps OS image"
    
    Update bootimage, emmcload and sd1load to place the FDT at
    0xC8000000, safely above the kernel end and within the valid DRAM
    region. No functional changes other than correcting the load
    addresses.
    
    Signed-off-by: John Vincent <john.vincent.xa@bp.renesas.com>

--- include/configs/rzt2n-dev.h
+++ include/configs/rzt2n-dev.h
@@ -59,9 +59,9 @@
 	"bootm_size=0x10000000\0" \
 	"prodsdbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk1p2 \0" \
 	"prodemmcbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk0p2 \0" \
-	"bootimage=booti 0xc4200000 - 0xC5F00000\0" \
-	"emmcload=ext4load mmc 0:2 0xc4200000 boot/Image;ext4load mmc 0:2 0xC5F00000 boot/r9a07g076m43-dev.dtb;run prodemmcbootargs \0" \
-	"sd1load=ext4load mmc 1:2 0xc4200000 boot/Image;ext4load mmc 1:2 0xC5F00000 boot/r9a07g076m43-dev.dtb;run prodsdbootargs \0" \
+	"bootimage=booti 0xc4200000 - 0xC8000000\0" \
+	"emmcload=ext4load mmc 0:2 0xc4200000 boot/Image;ext4load mmc 0:2 0xC8000000 boot/r9a07g076m43-dev.dtb;run prodemmcbootargs \0" \
+	"sd1load=ext4load mmc 1:2 0xc4200000 boot/Image;ext4load mmc 1:2 0xC8000000 boot/r9a07g076m43-dev.dtb;run prodsdbootargs \0" \
 	"bootcmd_check=if mmc dev 1; then run sd1load; else run emmcload; fi \0"
 
 #define CONFIG_BOOTCOMMAND	"env default -a;run bootcmd_check;run bootimage"
